80d4f52c9c73a2548f918f88132d57b2a11d7bd7
Harpo Barrett <61974761+hbar0009@users.noreply.github.com>
2022-03-31 13:42:26 +1100

    CU-22bhxp0_Fix-test-configuration (#2)
    * #22bhxp0 Removed playwright from dependencies
    
    * #22bhxp0 Added testing frameworks to dependencies
    
    Added: jest ts-jest svelte-jester @types/jest @testing-library/svelte
    
    * #22bhxp0 Removed old playwright test file
    
    * #22bhxp0[review] Configure Jest and Svelte testing library

---
+++ __tests__/Index.test.ts
@@ -0,0 +1,15 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/svelte";
+import Index from "../src/routes/index.svelte";
+
+describe("Test index renders correctly", () => {
+    test("Welcome text is present", () => {
+        const { getByText } = render(Index);
+
+        expect(getByText("Welcome to SvelteKit")).toBeInTheDocument();
+    });
+});
