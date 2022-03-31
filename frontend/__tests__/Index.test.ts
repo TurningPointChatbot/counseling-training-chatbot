ce115fba1653e1f77b76c8aaf37235b477083ac3
Harpo Barrett <hbar0009@student.monash.edu>
2022-04-01 09:26:08 +1100

    style: configure prettier and apply styles
    #22q1rm5[done]

--- frontend/__tests__/Index.test.ts
+++ frontend/__tests__/Index.test.ts
@@ -2,14 +2,14 @@
  * @jest-environment jsdom
  */
 
-import "@testing-library/jest-dom";
-import { render } from "@testing-library/svelte";
-import Index from "../src/routes/index.svelte";
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/svelte';
+import Index from '../src/routes/index.svelte';
 
-describe("Test index renders correctly", () => {
-    test("Welcome text is present", () => {
-        const { getByText } = render(Index);
+describe('Test index renders correctly', () => {
+  test('Welcome text is present', () => {
+    const { getByText } = render(Index);
 
-        expect(getByText("Welcome to SvelteKit")).toBeInTheDocument();
-    });
+    expect(getByText('Welcome to SvelteKit')).toBeInTheDocument();
+  });
 });
