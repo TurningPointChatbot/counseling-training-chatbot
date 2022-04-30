193ed47f45cf33cbb6a84bd39abc231b9f11bf00
Jason Tay <jtay0034@student.monash.edu>
2022-04-30 17:11:24 +1000

    Made the navbar, user button and side drawer

--- frontend/src/routes/adminBar.svelte
+++ frontend/src/routes/adminBar.svelte
@@ -5,26 +5,14 @@
 
 <script lang="ts">
     import {Nav, Navbar, NavItem, NavLink,NavbarToggler,Collapse,Icon,Dropdown,DropdownMenu,DropdownToggle,DropdownItem} from 'sveltestrap';
+
+    let isOpen = false;
 </script>
 
-<Navbar>
-    <NavbarToggler/>
-    <Collapse navbar>
-        <Nav navbar>
-            <NavItem>
-                <NavLink>Dashboard</NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink>Counsellors</NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink>Training Modules</NavLink>
-            </NavItem>
-        </Nav>
-    </Collapse>
-    <NavItem>
+<Navbar style="background-color:darkviolet">
+    <NavbarToggler on:click={() => (isOpen = !isOpen)}/>
     <Dropdown>
-        <DropdownToggle>
+        <DropdownToggle style="border-radius:50%">
         <Icon name="person-circle"/>
         </DropdownToggle>
         <DropdownMenu>
@@ -32,15 +20,21 @@
                 Administrator<br>
                 <h1>Name</h1>
             </DropdownItem>
-            <DropdownItem><p>Logout<Icon name="arrow-return-right"/></p></DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem>Logout <Icon name="arrow-return-right"/></DropdownItem>
         </DropdownMenu>
     </Dropdown>
-    </NavItem>
 </Navbar>
-<style>
-    .drawer-container {
-        position: relative;
-        display: flex;
-        height: 350px;
-    }
-</style>
+<Collapse {isOpen} style="background-color:#f8f9fa;width:20%;height:100%;padding-left:25px">
+    <Nav navbar>
+        <NavItem>
+            <NavLink>Dashboard</NavLink>
+        </NavItem>
+        <NavItem>
+            <NavLink>Counsellors</NavLink>
+        </NavItem>
+        <NavItem>
+            <NavLink>Training Modules</NavLink>
+        </NavItem>
+    </Nav>
+</Collapse>
