61f87ba12f9913147a30819b47a4322545f3d16f
cody <codysu2011@gmail.com>
2022-05-02 15:15:33 +1000

    rebase account and change password to rc

---
+++ frontend/src/routes/password.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import { Input, Button } from 'sveltestrap/src';
+
+  function savePassword() {
+		console.log("TODO")
+	}
+
+</script>
+
+<div class="content">
+
+  <h2>Change Password</h2>
+
+  <hr class="my-4"/>
+
+  <div class="row">
+    <Input type="password" class="form-control col-2 w-25 m-2" placeholder="New Password"/>
+    <Input type="password" class="form-control col-2 w-25 m-2" placeholder="Confirm New Password"/>
+  </div>
+  <div class="row">
+    <Input type="password" class="form-control col-2 w-25 m-2" placeholder="Current Password"/>
+    <Button class="col-1 m-2" on:click={savePassword}>
+      Save
+    </Button>
+  </div>
+
+</div>
+
+<style>
+  .content {
+    margin: 50px;
+  }
+</style>
