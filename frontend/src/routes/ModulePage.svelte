5ccefb3dcfcc064247894302ae5894de878b2bc2
Dylan Liu <dylliu0209@hotmail.com>
2022-05-07 00:09:57 +1000

    include incomplete module

--- frontend/src/routes/ModulePage.svelte
+++ frontend/src/routes/ModulePage.svelte
@@ -12,13 +12,31 @@
     </div>
 </div>
 
-<div class="grid grid-rows-2 grid-flow-col gap-8">
+<div class="grid-flow-col gap-8 my-4">
+    <div tabindex="0" class="collapse collapse-arrow border border-base-300 bg-base-300 rounded-box" style="color:black">
+        <input type="checkbox"/> 
+        <div class="collapse-title text-2xl font-medium">
+            <div class="grid grid-cols-3 gaps-4">
+                <div>Module 2</div>
+                <div>12/05/2022</div>
+                <div>
+                    <!-- Add progress bar here -->
+                </div>
+            </div>
+        </div>
+        <div class="collapse-content"> 
+            <h2><b> Description: This is the description for incomplete module 2</b></h2> 
+        </div>
+    </div>
+</div>
+
+<div class="grid-flow-col gap-8 my-4">
     <div tabindex="0" class="collapse collapse-arrow border border-base-300 bg-base-300 rounded-box" style="color:black">
         <input type="checkbox"/> 
         <div class="collapse-title text-2xl font-medium">
             <div class="grid grid-cols-3 gaps-4">
                 <div>Module 1</div>
-                <div>12/05/2022</div>
+                <div>06/05/2022</div>
                 <div>
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -30,5 +48,4 @@
             <h2><b> Description: This is the description for completed module 1</b></h2> 
         </div>
     </div>
-    
 </div>
