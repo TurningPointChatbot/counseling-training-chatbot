e3326696d7641e1211822dc0df983ecb9296b7a3
cody <codysu2011@gmail.com>
2022-05-07 00:45:34 +1000

    fix: add heading styles
    Tailwind defaults to removing styles for headings (and other things like lists) to follow modern mormalize. https://tailwindcss.com/docs/preflight
    
    I believe the convention is to keep preflight on and to overwriet with your own styling.
    
    So we should decide what our base styles are and also what our theme should be, according to this doc: https://tailwindcss.com/docs/adding-custom-styles

--- frontend/src/routes/daisy-spike.svelte
+++ frontend/src/routes/daisy-spike.svelte
@@ -1,3 +1,6 @@
+<h1>heading 1</h1>
+<h2>heading 2</h2>
+
 <button
   class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900"
   >Button</button
