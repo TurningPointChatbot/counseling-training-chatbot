0670e487f1afa5d6d24f37f614f8e195fdf26d6b
Brent <bdia0003@student.monash.edu>
2022-05-07 21:23:43 +1000

    feat: added tailwind progress bar for incomplete modules.

--- frontend/src/routes/ModulePage.svelte
+++ frontend/src/routes/ModulePage.svelte
@@ -22,7 +22,10 @@
                 <div>Module 2</div>
                 <div>12/05/2022</div>
                 <div>
-                    <!-- Add progress bar here -->
+                    <!--Progress bar here -->
+                    <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                        <div class="bg-blue-400 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style="width: 45%"> 45%</div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -33,6 +36,44 @@
                 This is the description for incomplete module 2.
                 This is the description for incomplete module 2.
                 This is the description for incomplete module 2.
+            </b></h2>
+            <div class = "text-right">
+                <button class="btn-highlight btn-icon">
+                    <div class="svgicon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="80" height="60" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
+                          </svg>
+                    </div>
+                    <b>Run Module</b>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--Incomplete Module-->
+<div class="grid-flow-col gap-8 my-4">
+    <div tabindex="0" class="collapse collapse-arrow border border-base-300 bg-base-300 rounded-box" style="color:black">
+        <input type="checkbox"/> 
+        <div class="collapse-title text-2xl font-medium">
+            <div class="grid grid-cols-3 gaps-4">
+                <div>Module 3</div>
+                <div>18/05/2022</div>
+                <div>
+                    <!-- Progress bar here -->
+                    <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                        <div class="bg-blue-400 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style="width: 10%"> 10%</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="collapse-content"> 
+            <h2><b> Description: This is the description for incomplete module 3.
+                This is the description for incomplete module 3.
+                This is the description for incomplete module 3.
+                This is the description for incomplete module 3.
+                This is the description for incomplete module 3.
+                This is the description for incomplete module 3.
             </b></h2>
             <div class = "text-right">
                 <button class="btn-highlight btn-icon">
