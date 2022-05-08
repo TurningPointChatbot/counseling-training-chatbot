6b22ff458b3311dd5827c674ab61ea5799b91bcd
Brent <bdia0003@student.monash.edu>
2022-05-08 16:27:23 +1000

    feat: implemented messaging page's header and textbox components.

---
+++ frontend/src/routes/MessagingPage.svelte
@@ -0,0 +1,30 @@
+<html data-theme="cupcake"></html>
+
+<!-- Header -->
+<div class="flex justify-center">
+    <div class="block p-6 mt-8 mb-8 mx-10 rounded shadow-lg bg-gray-300 w-full">
+      <h5 class="text-gray-900 text-xl leading-tight font-medium">Module Name</h5>
+    </div>
+</div>
+
+<div class="flex justify-center">
+  <div class="mb-3 xl:w-full mx-10">
+    <!-- Buttons for text-->
+    <div class="block p-6  rounded shadow-lg bg-gray-300 w-full">
+        <button type="button" class="rounded inline-block px-6 py-2.5 bg-gray-600 text-white font-medium text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out">1</button>
+        <button type="button" class="rounded inline-block px-6 py-2.5 bg-gray-600 text-white font-medium text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out">2</button>
+        <button type="button" class="rounded inline-block px-6 py-2.5 bg-gray-600 text-white font-medium text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out">3</button>
+        <button type="button" class="rounded inline-block px-6 py-2.5 bg-gray-600 text-white font-medium text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out">4</button>
+        <button type="button" class="rounded inline-block px-6 py-2.5 bg-gray-600 text-white font-medium text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out">5</button>
+        <button type="button" class="rounded inline-block px-6 py-2.5 bg-red-600 justify-end text-white font-medium text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out">End Chat</button>
+    </div>
+    <!-- Text area -->
+    <textarea
+      class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border 
+        border-solid border-gray-300 rounded transition ease-in-out focus:text-gray-700 focus:bg-white 
+        focus:border-blue-600 focus:outline-none "
+      id="exampleFormControlTextarea1"
+      rows="3"
+      placeholder="Enter message here..."></textarea>
+  </div>
+</div>
--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,5 +1,7 @@
 <script lang='ts'>
   import ModulePage from './ModulePage.svelte';
+  import LearningOutcomePage from './LearningOutcomePage.svelte';
+  import MessagingPage from './MessagingPage.svelte';
 </script>
 
-<ModulePage/>
+<MessagingPage/>
