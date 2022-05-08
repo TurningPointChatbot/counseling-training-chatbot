94123d1c5cf1b55c2374fbe37c780ecce177ea9f
Dylan Liu <dylliu0209@hotmail.com>
2022-05-08 18:07:45 +1000

    feat: implement chat interface

--- frontend/src/routes/MessagingPage.svelte
+++ frontend/src/routes/MessagingPage.svelte
@@ -6,8 +6,54 @@
       <h5 class="text-gray-900 text-xl leading-tight font-medium">Module Name</h5>
     </div>
 </div>
-
-<div class="flex justify-center">
+<div class="container px-10 w-[100rem]">
+  <div class="max-w-full border rounded">
+    <div class="tabs">
+      <a class="tab tab-lg tab-bordered tab-active">Conversation</a> 
+      <a class="tab tab-lg tab-bordered">Chats </a> 
+      <a class="tab tab-lg tab-bordered">Details </a>
+      <a class="tab tab-lg tab-bordered">Email</a>
+    </div>
+    <div>
+      <div class="w-full">
+        <div class="relative flex items-center p-3 border-b border-gray-300">
+          <img class="object-cover w-10 h-10 rounded-full"
+            src="https://cdn.pixabay.com/photo/2018/01/15/07/51/woman-3083383__340.jpg" alt="username" />
+          <span class="block ml-2 font-bold text-gray-600">Emma</span>
+          <span class="absolute w-3 h-3 bg-green-600 rounded-full left-10 top-3">
+          </span>
+        </div>
+        <div class="relative w-full p-6 overflow-y-auto h-[31rem]">
+        <div class="overflow-auto ...">
+          <ul class="space-y-2">
+            <li class="flex justify-start">
+              <div class="relative max-w-xl px-4 py-2 text-gray-700 rounded shadow">
+                <span class="block">Hi</span>
+              </div>
+            </li>
+            <li class="flex justify-end">
+              <div class="relative max-w-xl px-4 py-2 text-gray-700 bg-gray-100 rounded shadow">
+                <span class="block">Hiiii</span>
+              </div>
+            </li>
+            <li class="flex justify-end">
+              <div class="relative max-w-xl px-4 py-2 text-gray-700 bg-gray-100 rounded shadow">
+                <span class="block">how are you?</span>
+              </div>
+            </li>
+            <li class="flex justify-start">
+              <div class="relative max-w-xl px-4 py-2 text-gray-700 rounded shadow">
+                <span class="block">Lorem ipsum dolor sit, amet consectetur adipisicing elit. </span>
+              </div>
+            </li>
+          </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="flex justify-center my-4">
   <div class="mb-3 xl:w-full mx-10">
     <!-- Buttons for text-->
     <div class="block p-6  rounded shadow-lg bg-gray-300 w-full">
