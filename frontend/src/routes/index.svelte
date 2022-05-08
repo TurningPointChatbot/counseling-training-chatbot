03e7e165b16cf318a1d5ac72253337a00a966003
Damien Jap <dami0004@student.monash.edu>
2022-05-09 05:23:11 +1000

    fix import reference

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import AdminBar from "../lib/components/AdminBar.svelte";
   import Dashboard from "./admin/dashboard.svelte";
-  import DashboardModules from "./admin/DashboardModules.svelte";
+  import DashboardModules from "./admin/dashboardModules.svelte";
 </script>
 <AdminBar><Dashboard/><DashboardModules/></AdminBar>
