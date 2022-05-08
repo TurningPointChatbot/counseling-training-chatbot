ae3c9960dfb3cd7e4ae8d5110d305a7fc6fea810
Katherine Moffat <kmof0001@student.monash.edu>
2022-05-08 22:01:21 +1000

    fixed up style a bit and added text as buttons

--- frontend/src/routes/admin/dashboard.svelte
+++ frontend/src/routes/admin/dashboard.svelte
@@ -23,33 +23,26 @@
 
 <!doctype html>
 <html lang="en">
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <!-- Tailwind CSS -->
-  <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-
-</head>
 
 <body>
-  <div class="font-bold text-xl mb-2">Counsellors</div>
-  <p class="text-gray-700 text-base">
+  <div class="font-bold text-xl mb-2 text-gray-700">Counsellors</div>
+  <p class="text-gray-400 text-base">
     Recently viewed
 </p>
-  <div class="flex items-center rounded bg-white overflow-hidden mt-10 mr-8 hover:shadow-lg transform transition hover:-translate-y-1">
+  <div class="flex items-center rounded bg-white overflow-hidden h-90 w-90">
     {#each counsellorPreview as cardItem}
     <!--Card 1-->
-    <div class="rounded shadow-lg">
-      <img class="rounded-full" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" alt="Davos Sand">
+    <div class="rounded">
+      <img class="rounded-full h-32 w-32" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" alt="Davos Sand">
       <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">{cardItem.title}</div>
+        <div class="font-bold text-xl mb-2">
+          <button type="button" class="font-small text-gray-900 bg-white rounded-full focus:z-10 dark:bg-gray-800 dark:text-gray-400 dark:hover:text-black center">{cardItem.title}</button>
+        </div>
       </div>
     </div>
     {/each}
     <div class="flex-direction: row; rounded-full ">
-        <button type="button" class="text-grey-700 border border-grey-700 hover:bg-grey-700 hover:text-black focus:ring-4 focus:outline-none focus:ring-grey-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-grey-500 dark:text-grey-500 dark:hover:text-black dark:focus:ring-grey-800">
+        <button type="button" class="text-grey-700 border border-grey-700 hover:bg-grey-700 hover:text-black focus:ring-1 focus:ring-grey-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-grey-500 dark:text-grey-500 dark:hover:text-black dark:focus:ring-grey-800">
             <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
           </button>
       </div>
