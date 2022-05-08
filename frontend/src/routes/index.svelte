b5c21e9a84da52aa884e37316bac9d14f8a73d8d
Damien Jap <dami0004@student.monash.edu>
2022-05-09 05:17:05 +1000

    feat: implement dashboard modules

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import AdminBar from "../lib/components/AdminBar.svelte";
   import Dashboard from "./admin/dashboard.svelte";
+  import DashboardModules from "./admin/DashboardModules.svelte";
 </script>
-<AdminBar><Dashboard/></AdminBar>
+<AdminBar><Dashboard/><DashboardModules/></AdminBar>
