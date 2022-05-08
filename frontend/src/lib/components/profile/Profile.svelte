aa1a1210e34cb14c29a933cba6305e321990188b
Harpo Barrett <hbar0009@student.monash.edu>
2022-05-08 13:36:31 +1000

    refactor: extract user details into variables and bind inputs

--- frontend/src/lib/components/profile/Profile.svelte
+++ frontend/src/lib/components/profile/Profile.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
+  // NOTE: much of this code was adapted from supabase's official docs
+  // https://supabase.com/docs/guides/with-svelte
+  // last accessed: 08/05/2022
   import AvatarCard from '$lib/components/profile/AvatarCard.svelte';
 
   let editOn = false;
 
+  let fullName: string = 'John Smith';
+  let mobile: string = '0404 333 222';
+  let email: string = 'johnsmith@turningpoint.org.au';
+  let position: string = 'Junior Counsellor';
+
   function toggleEdit() {
     editOn = !editOn;
+  }
+
+  async function getUserInfo() {
+    try {
+    } catch (error) {}
   }
 </script>
 
@@ -22,33 +35,48 @@
   <div class="row justify-content-center mx-2 my-4">
     <p class="col-md-2 col-md-offset-3 fw-bold">Full Name</p>
     {#if !editOn}
-      <p class="col-md-6 col-md-offset-3">John Smith</p>
+      <p class="col-md-6 col-md-offset-3">{fullName}</p>
     {/if}
     {#if editOn}
-      <input class="col-md-6 col-md-offset-3 w-50" placeholder="username" />
+      <input
+        class="col-md-6 col-md-offset-3 w-50"
+        placeholder="Full Name"
+        type="text"
+        bind:value={fullName}
+      />
     {/if}
   </div>
   <div class="row justify-content-center mx-2 my-4">
     <p class="col-md-2 col-md-offset-3 fw-bold">Mobile</p>
     {#if !editOn}
-      <p class="col-md-6 col-md-offset-3">0404 333 222</p>
+      <p class="col-md-6 col-md-offset-3">{mobile}</p>
     {/if}
     {#if editOn}
-      <input class="col-md-6 col-md-offset-3 w-50" placeholder="mobile" />
+      <input
+        class="col-md-6 col-md-offset-3 w-50"
+        placeholder="mobile"
+        type="text"
+        bind:value={mobile}
+      />
     {/if}
   </div>
   <div class="row justify-content-center mx-2 my-4">
     <p class="col-md-2 col-md-offset-3 fw-bold">Email</p>
     {#if !editOn}
-      <p class="col-md-6 col-md-offset-3">johnsmith@turningpoint.org.au</p>
+      <p class="col-md-6 col-md-offset-3">{email}</p>
     {/if}
     {#if editOn}
-      <input class="col-md-6 col-md-offset-3 w-50" placeholder="email" />
+      <input
+        class="col-md-6 col-md-offset-3 w-50"
+        placeholder="email"
+        type="text"
+        bind:value={email}
+      />
     {/if}
   </div>
   <div class="row justify-content-center mx-2 my-4">
     <p class="col-md-2 col-md-offset-3 fw-bold">Position</p>
-    <p class="col-md-6 col-md-offset-3">Junior Counsellor</p>
+    <p class="col-md-6 col-md-offset-3">{position}</p>
   </div>
 
   <div class="flex flex-row-reverse space-x-4 space-x-reverse">
