086c5ca026568f3d3260fcae4305741a413b336d
Linton Charles <lintoncharles2000@gmail.com>
2022-05-11 08:50:10 +1000

    fix: end chat button alignment & body-header alignment

--- frontend/src/routes/chatbot-simulation.svelte
+++ frontend/src/routes/chatbot-simulation.svelte
@@ -18,8 +18,9 @@
     messages.push({sender: "counsellor", content: "okay"})
 </script>
 
-<html data-theme="cupcake"></html>
+<html lang="en" data-theme="cupcake"></html>
 
+<div class="w-5/6 mx-28">
 <!-- Header -->
 <div class="flex justify-center">
     <div class="block p-6 mt-8 mb-8 mx-10 rounded shadow-lg bg-gray-300 w-full">
@@ -58,15 +59,18 @@
 <div class="flex justify-center my-4">
   <div class="mb-3 w-full mx-10">
     <!-- Buttons for text-->
-    <div class="block py-2 px-4 rounded-t justify-items-end shadow-lg bg-gray-300 w-full">
+    <div class="block py-2 px-4 rounded-t shadow-lg bg-gray-300 w-full">
+      <!-- <div> -->
         <button type="button" class="rounded inline-block px-6 py-2.5 bg-gray-600 text-white font-medium text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out">1</button>
         <button type="button" class="rounded inline-block px-6 py-2.5 bg-gray-600 text-white font-medium text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out">2</button>
         <button type="button" class="rounded inline-block px-6 py-2.5 bg-gray-600 text-white font-medium text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out">3</button>
         <button type="button" class="rounded inline-block px-6 py-2.5 bg-gray-600 text-white font-medium text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out">4</button>
         <button type="button" class="rounded inline-block px-6 py-2.5 bg-gray-600 text-white font-medium text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out">5</button>
-        <button on:click={returnToModules} type="button" style="margin-left: 70%;" class="rounded inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out">End Chat</button>
-  </div>
-  
+      <!-- </div>
+      <div class="float-right"> -->
+        <button on:click={returnToModules} type="button" class="rounded inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out float-right">End Chat</button>
+      <!-- </div> -->
+    </div>
     <!-- Text area -->
     <textarea
       class="form-control block w-full px-4 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border 
@@ -76,4 +80,5 @@
       rows="3"
       placeholder="Enter message here..."></textarea>
   </div>
+</div>
 </div>
