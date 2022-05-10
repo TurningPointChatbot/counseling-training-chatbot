b0e92d325259d32dff950c0559535eb642af94b0
Linton Charles <lintoncharles2000@gmail.com>
2022-05-10 10:16:50 +1000

    feat: implement page routing

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,7 +1,18 @@
 <script lang='ts'>
+  import {Router, Route} from "svelte-routing"
+
   import ModulePage from './ModulePage.svelte';
   import LearningOutcomePage from './LearningOutcomePage.svelte';
   import MessagingPage from './MessagingPage.svelte';
 </script>
 
-<ModulePage/>
+
+<Router>
+
+  <Route path="/" component="{ModulePage}"/>
+  
+  <Route path="learning-outcomes" component="{LearningOutcomePage}"/>
+
+  <Route path="chatbot-simuation" component="{MessagingPage}"/>
+
+</Router>
