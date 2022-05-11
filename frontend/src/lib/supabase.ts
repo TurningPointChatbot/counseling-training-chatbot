beca108f140025b8314d092236c1ad913f70ba8c
Harpo Barrett <hbar0009@student.monash.edu>
2022-05-11 15:46:23 +1000

    fix: remove api key literals

--- frontend/src/lib/supabase.ts
+++ frontend/src/lib/supabase.ts
@@ -1,15 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  <string>'https://wkmzrfsgsgmxlbufzffk.supabase.co',
-  <string>(
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndrbXpyZnNnc2dteGxidWZ6ZmZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDg2OTQ5NjAsImV4cCI6MTk2NDI3MDk2MH0.LLBrYL1vmpjNNruTykkvZuqwxZzit4NAtSomZT9TZ-w'
-  )
+  <string>import.meta.env.VITE_SUPABASE_URL,
+  <string>import.meta.env.VITE_SUPABASE_ANON_KEY
 );
 
-/*const supabase = createClient(
-    <string>import.meta.env.VITE_SUPABASE_URL,
-    <string>import.meta.env.VITE_SUPABASE_ANON_KEY
-);
-*/
 export default supabase;
