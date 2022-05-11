c6974dd0ab6099279c139e68889b6ff072d0f010
Angus <angus1angus@bigpond.com>
2022-05-11 15:26:41 +1000

    Connected Login Page to database and added password reset function

--- frontend/src/lib/supabase.ts
+++ frontend/src/lib/supabase.ts
@@ -1,8 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  <string>import.meta.env.VITE_SUPABASE_URL,
-  <string>import.meta.env.VITE_SUPABASE_ANON_KEY
+  <string>"https://wkmzrfsgsgmxlbufzffk.supabase.co",
+  <string>"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndrbXpyZnNnc2dteGxidWZ6ZmZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDg2OTQ5NjAsImV4cCI6MTk2NDI3MDk2MH0.LLBrYL1vmpjNNruTykkvZuqwxZzit4NAtSomZT9TZ-w"
 );
 
+/*const supabase = createClient(
+    <string>import.meta.env.VITE_SUPABASE_URL,
+    <string>import.meta.env.VITE_SUPABASE_ANON_KEY
+);
+*/
 export default supabase;
