9dab2651057fe66e530746e847fe426307bb2ac3
Anna Giang <49330921+anna-giang@users.noreply.github.com>
2022-05-12 23:55:50 +1000

    feat: administrator dashboard, counsellor and module views (#17)
    * CU-251f194 - Administrator dashboard side menu, header and login/logout
    
    * CU-251f194 - Administrator dashboard side menu, header and login/logout
    
    * feat: implement ui for counsellor list page
    
    includes the HTML and styles for the list, and TODOs where additional
    functionality is to be implemented in future.
    
    * feat: implement sort counsellor list function
    
    counsellor list will be sorted by counsellor names on click of the sort
    icon. also blur sort icon on hover to better indicate that it is click-able.
    
    * feat: implement filtering of counsellor list by user-inputted term
    
    as the user inputs the search term, filter the counsellor list by
    searching for the term (ignoring case) in counsellor names and
    counsellor positions.
    
    * style: format counsellor-list.svelte
    
    run the formatter on the file, as some code lines were too long, and fix
    whitespace.
    
    * Made the navbar, user button and side drawer
    
    * placed links in head element in app.html for bootstrap
    
    * chore: install tailwindcss
    
    * chore:: install daisyui dependency
    
    * feat: add daisy-spike page
    
    * chore: run prettier
    
    * adjusted admin bar to use daisy ui
    
    * build: remove sveltestrap from package and package-lock
    
    as per announcement on discord, we are no longer using sveltestrap. the
    sveltestrap (and other unused dependencies) were re-added back in when we merged the branch with rc.
    
    * fix: switch to tailwind in counsellor list
    
    also convert counsellor list code into a component
    
    * style: run prettier on AdminBar file and rename file
    
    components should start with capital
    
    * refactor: generalise the counsellor list
    
    instead of a 'CounsellorList' component, we will have a 'FilterableList'
    component that can be used across both the 'Counsellors' page and the
    'Modules' page.
    
    * feat: implement 'Modules' page
    
    also change the images in the FilterableList to a rounded-rectangle
    shape as the default.
    
    * dashboard list of counsellors implementation
    
    first attempt at councellor list on dashboard still needs styling and components extracted
    
    * made counsellor pictures rounded
    
    made pictures rounded
    
    * fixed up style a bit and added text as buttons
    
    * Made counsellor page and modules page to put frontend code together for naviagtion later
    
    * feat: implement dashboard modules
    
    * fix import reference
    
    * changed icon of account button on header
    
    * refactor: clean-up the admin routes and combine code
    
    also use formatter to clean-up code style.
    
    * feat: add navigation links to link the admin pages
    
    link the 'See all' buttons and side menu items to their corresponding
    pages.
    
    * feat: refine styling of the admin pages
    
    * Index had dashboard gone so i placed it back in
    
    * removed tiny dash in the index.html
    
    * Made changes according to pull request
    
    * fixed import issues in the files
    
    * moved components out of admin folder except AdminBar
    
    * chore: run prettier
    
    Co-authored-by: Jason Tay <jtay0034@student.monash.edu>
    Co-authored-by: Harpo Barrett <hbar0009@student.monash.edu>
    Co-authored-by: Katherine Moffat <kmof0001@student.monash.edu>
    Co-authored-by: Damien Jap <dami0004@student.monash.edu>

---
+++ frontend/src/lib/components/RecentModules.svelte
@@ -0,0 +1,101 @@
+<script lang="ts">
+  export let listData: any[];
+
+  let sortedList = listData;
+
+  // sort from most recently accessed to least recently accessed module
+  function sortMostRecent() {
+    sortedList.sort(function (a, b) {
+      return b.dateAccessed.getTime() - a.dateAccessed.getTime();
+    });
+    if (sortedList.length > 4) {
+      sortedList = sortedList.slice(0, 4); // only displaying max 4 modules at once on dash
+    }
+  }
+  sortMostRecent();
+</script>
+
+<div class="card m-3">
+  <div id="title" class="font-bold text-xl mb-2 text-gray-700">
+    Training Modules
+  </div>
+  <div id="subtitle" class="text-gray-400 text-base">Recently viewed</div>
+  <div
+    id="modules"
+    class="flex items-center bg-white overflow-hidden mt-4 mr-8 transform transition"
+  >
+    {#each sortedList as cardItem}
+      <!--Card 1-->
+      <div
+        class="flex flex-col items-center pr-5 overflow-hidden hover:-translate-y-1"
+      >
+        <div>
+          <img
+            class="rounded-rectangle"
+            src={cardItem.image}
+            alt={cardItem.title}
+          />
+        </div>
+        <div class="flex py-4">
+          <div class="text-xl mb-2">
+            {cardItem.title}
+          </div>
+        </div>
+      </div>
+    {/each}
+    <div id="see_all" class="flex flex-col justify-center">
+      <a href="/admin/modules" rel="prefetch" class="nav-link">
+        <button
+          type="button"
+          class="text-grey-700 border border-grey-700 hover:bg-grey-700 hover:text-black focus:ring-4 focus:outline-none focus:ring-grey-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-grey-500 dark:text-grey-500 dark:hover:text-black dark:focus:ring-grey-800"
+        >
+          <svg
+            class="w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+            ><path
+              fill-rule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            /></svg
+          >
+        </button>
+      </a>
+      <div class="text-gray-500 text-base text-center">See all</div>
+    </div>
+  </div>
+</div>
+
+<style>
+  #modules {
+    position: relative;
+    left: 20px;
+  }
+
+  .rounded-rectangle {
+    /* Crops the image to a rectangle with rounded corners. */
+    object-fit: cover;
+    border-radius: 25px;
+    height: 180px;
+    width: 250px;
+    position: relative;
+  }
+
+  #subtitle {
+    position: relative;
+    left: 30px;
+    top: 5px;
+  }
+
+  #title {
+    position: relative;
+    left: 30px;
+    top: 10px;
+  }
+
+  #see_all {
+    position: relative;
+    left: 25px;
+  }
+</style>
--- frontend/src/lib/components/overallProgress.svelte
+++ frontend/src/lib/components/overallProgress.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-    let modPrint = "Completed : 10 / 40";
-    let timePrint = "Time Remaining : 50 Hrs";
+  let modPrint = 'Completed : 10 / 40';
+  let timePrint = 'Time Remaining : 50 Hrs';
 </script>
 
-<br><br>
+<br /><br />
 <center>
   <div class="card bg-base-100 shadow-xl">
     <div class="card-title justify-left bg-base-400">
@@ -12,14 +12,24 @@
     <div class="card-body text-left">
       Module Completion
       <div class="w-full bg-gray-200 rounded-full">
-        <div class="bg-secondary text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded" style="width: 25%">25%</div>
+        <div
+          class="bg-secondary text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded"
+          style="width: 25%"
+        >
+          25%
+        </div>
       </div>
-      <div class = "font-xs text-base-400">{modPrint}</div>
+      <div class="font-xs text-base-400">{modPrint}</div>
       Target Training Time
       <div class="w-full bg-gray-200 rounded-full">
-        <div class="bg-secondary text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded" style="width: 88%">88%</div>
+        <div
+          class="bg-secondary text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded"
+          style="width: 88%"
+        >
+          88%
+        </div>
       </div>
-      <div class = "font-xs text-base-400">{timePrint}</div>
+      <div class="font-xs text-base-400">{timePrint}</div>
     </div>
   </div>
 </center>
