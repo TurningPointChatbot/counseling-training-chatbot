79f6aaff9cd47b91c5625f3affe2f1bfabfa1391
Ariel Chandra <88024997+LuckyCharm00@users.noreply.github.com>
2022-05-12 15:34:59 +1000

    Update index.svelte

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,4 +1,10 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<!--This is ariel's and wil's main page code-->
+<!-- <script>
+  import ModuleBigCard from "../lib/components/moduleBigCard.svelte";
+</script>
+
+<svelte:head>
+  <title>Trial</title>
+</svelte:head>
+
+<ModuleBigCard moduleBigCard = {ModuleBigCard}/> -->
