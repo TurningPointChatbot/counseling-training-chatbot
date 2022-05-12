a37b734138f23ab9a584ce273753e1874522688a
nkar <nkar0022@student.monash.edu>
2022-05-12 17:00:43 +1000

    Modified the Overal Progress widget for the given layout

--- frontend/src/lib/components/moduleOverallProgress.svelte
+++ frontend/src/lib/components/moduleOverallProgress.svelte
@@ -1,10 +1,25 @@
 <script lang="ts">
-    let modPrint = "Completed : 10 / 40";
-    let timePrint = "Time Remaining : 50 Hrs";
+    import TimeRemaining from "../components/timeRemaining.svelte";
+    import ModProgress from "../components/modProgress.svelte";
     export let ModuleOverallProgress;
 </script>
 
 <div class="card bg-base-100 shadow-xl">
+    <div>
+        <h2 class="card-title p-2 text-3xl self-center align-center bg-base-300">Overall Progress</h2>
+    </div>
+    <hr class="bg-base-400">
+    <div class="card-body grid grid-row gap-3 w-full">
+        <div class="card-body grid grid-cols-20 gap-3 w-full">
+            <div class="row"><ModProgress moduleProgress = {ModProgress}/></div>
+        </div>
+        <div class="card-body grid grid-cols-20 gap-3 w-full">
+            <div class="row"><TimeRemaining timeRemaining = {TimeRemaining}/></div>
+        </div>
+    </div>
+</div>
+
+<!-- <div class="card bg-base-100 shadow-xl">
     <div>
         <h2 class="card-title p-2 text-3xl self-center align-center bg-base-300">Overall Progress</h2>
     </div>
@@ -22,4 +37,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
+
