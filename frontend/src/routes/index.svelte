5f12df373607e4f4c598261c1cbc9db4ed5af72e
Jason Tay <jtay0034@student.monash.edu>
2022-05-12 16:00:22 +1000

    Index had dashboard gone so i placed it back in

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,4 +1,5 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script lang="ts">
+  import Dashboard from "./admin/dashboard.svelte";
+</script>/
+
+<Dashboard/>
