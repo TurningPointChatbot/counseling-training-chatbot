08eb75cd33719a2f014629142242db0f067a2fcc
nkar <nkar0022@student.monash.edu>
2022-05-12 16:00:26 +1000

    Overall Progress card is added

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,12 +1,43 @@
 <!--This is ariel's and wil's main page code-->
 <script>
   import ModuleBigCard from "../lib/components/moduleBigCard.svelte";
+<<<<<<< Updated upstream
   import ModuleAssignedModuleCard from "../lib/components/moduleAssignedModuleCard.svelte";
+=======
+  let modPrint = "Completed : 10 / 40";
+  let timePrint = "Time Remaining : 50 Hrs";
+>>>>>>> Stashed changes
 </script>
 
 <svelte:head>
   <title>Trial</title>
 </svelte:head>
 
+<<<<<<< Updated upstream
 <ModuleAssignedModuleCard moduleAssignedModuleCard = {ModuleAssignedModuleCard}/>
 <ModuleBigCard moduleBigCard = {ModuleBigCard}/> 
+=======
+<ModuleBigCard moduleBigCard = {ModuleBigCard}/>
+
+<!--Navaneeth's Widget-->
+<br><br>
+<center>
+  <div class="card w-96 bg-base-100 shadow-xl">
+    <div class="card-title justify-left bg-base-400">
+      <h4>Overall Progress</h4>
+    </div>
+    <div class="card-body text-left">
+      Module Completion
+      <div class="w-full bg-gray-200 rounded-full">
+        <div class="bg-secondary text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded" style="width: 25%">25%</div>
+      </div>
+      <div class = "font-xs text-base-400">{modPrint}</div>
+      Target Training Time
+      <div class="w-full bg-gray-200 rounded-full">
+        <div class="bg-secondary text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded" style="width: 88%">88%</div>
+      </div>
+      <div class = "font-xs text-base-400">{timePrint}</div>
+    </div>
+  </div>
+</center>
+>>>>>>> Stashed changes
