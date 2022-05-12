554cc520233ae48930d6cb02dea8fb2d6a8825d6
samhoward-0007 <show0007@student.monash.edu>
2022-05-12 16:04:06 +1000

    changed cols to rows for assigned module section

--- frontend/src/lib/components/moduleAssignedModuleCard.svelte
+++ frontend/src/lib/components/moduleAssignedModuleCard.svelte
@@ -7,7 +7,7 @@
         <h2 class="card-title p-2 text-3xl self-center align-center bg-base-300">Assigned Modules</h2>
     </div>
     <hr class="bg-base-400">
-    <div class="card-body grid grid-cols-10 gap-3 w-full">
+    <div class="card-body grid grid-row gap-3 w-full">
         <p>2/5/2022</p><div class="col-span-3"><ModuleCard moduleCard = {ModuleCard}/></div>
         <p>2/5/2022</p><div class="col-span-3"><ModuleCard moduleCard = {ModuleCard}/></div>
     </div>
