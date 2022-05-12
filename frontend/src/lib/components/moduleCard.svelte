df4b3a9778f1c33418e616bcda8ca8c5536e479f
Ariel Chandra <88024997+LuckyCharm00@users.noreply.github.com>
2022-05-12 15:34:51 +1000

    Added a component moduleCard.svelte

---
+++ frontend/src/lib/components/moduleCard.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let ModuleCard;
+    import ModuleProgress from "./moduleProgress.svelte";
+  </script>
+  
+  <a class ="p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center" href="">
+      <img class="rounded-md" src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Random Picture">
+      <div><ModuleProgress moduleProgress={ModuleProgress}/></div>
+  </a>
