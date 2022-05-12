60d9efb866e1370c8297bff7e24539d44ca1a9ee
Irate Walrus <71628949+Irate-Walrus@users.noreply.github.com>
2022-05-12 08:36:25 +0000

    feat: counsellor dashboard template (#15)
    * added moduleProgress.svelte
    
    * added moduleCircleButton.svelte
    
    * Added a component moduleBigCard.svelte
    
    * Added a component moduleCard.svelte
    
    * Update index.svelte
    
    * Assigned Module Section using components
    
    * Removed progress bar from assigned module section
    
    * Overall Progress card is added
    
    * changed cols to rows for assigned module section
    
    * moved date into module card - Assigned Module page WIP
    
    * Added a component for the Overall Progress card widget
    
    * layouting
    
    * Modified the Overal Progress widget for the given layout
    
    * added margins
    
    * added comment
    
    * added lang typescript to components
    
    * fix: ensure correct passing of component parameters
    
    * fix: readd index.svelte
    
    Co-authored-by: wric0003 <wric0003@student.monash.edu>
    Co-authored-by: Ariel Chandra <88024997+LuckyCharm00@users.noreply.github.com>
    Co-authored-by: samhoward-0007 <show0007@student.monash.edu>
    Co-authored-by: nkar <nkar0022@student.monash.edu>

---
+++ frontend/src/lib/components/moduleProgress.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+</script>
+
+
+<!-- <progress class="progress  w-56 h-5 align-middle bg-base-400" value="70" max="100"></progress> -->
+<div class="w-96 h-6 bg-base-300 rounded-md m-3">
+    <div class="h-6 bg-blue-light text-sm font-medium text-white text-center p-1 leading-none rounded-md" style="width: 45%"> 45%</div>
+</div>
