6cfb01b2f8db7e4fed63661b3e3ae0db557b4619
linton2000 <lintoncharles2000@gmail.com>
2022-05-12 17:52:35 +1000

    feat: chatbot page frontend (#10)
    * module page initiation
    
    * implement completed module bar
    
    * implement moduletitle  header
    
    * grid rows to separate the modules
    
    * Corrected colours of header and module.
    
    * Fixed collapsing issue of header.
    
    * include incomplete module
    
    * Added button to run modules.
    
    * Fixed button colour.
    
    * modify run module button outlook
    
    * feat: added tailwind progress bar for incomplete modules.
    
    * style: Increased progress bar size and straightened edges.
    
    * feat: implement learning outcome page skeleton
    
    * feat: implemented messaging page's header and textbox components.
    
    * feat: implement chat interface
    
    * style: made all components the same width.
    
    * style: adjusted padding to make components start from same spot.
    
    * style: change learning outcome color to purple-700
    
    * feat: extract module rows into a component and implement props passsing
    
    * feat: implement page routing
    
    * refactor: rename routes to follow lowercase convention
    
    * feat: implement parameter passing from modules to learning-outcomes
    
    * fix: removed comma
    
    * feat: implement learning outcomes redirection
    
    * feat: end chat redirection
    
    * style: increase message chat size and adjust "end chat" button position
    
    * feat: progress and completed modules logic
    
    * feat: made ChatMessage component and implemented props passing.
    
    * fix: end chat button alignment & body-header alignment
    
    * chore: format project with prettier
    
    * fix: update chest_config
    
    * refactor: Replace svelte-routing with svelte-kit load functions
    
    * refactor: change index test to ModuleRow test
    
    * chore: run prettier
    
    Co-authored-by: Dylan Liu <dylliu0209@hotmail.com>
    Co-authored-by: Brent <bdia0003@student.monash.edu>
    Co-authored-by: Harpo Barrett <hbar0009@student.monash.edu>

---
+++ frontend/src/lib/components/ChatMessage.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export let sender: string;
+  export let content: string;
+</script>
+
+{#if sender == 'patient'}
+  <li class="flex justify-start">
+    <div class="relative max-w-xl px-4 py-2 text-gray-700 rounded shadow">
+      <span class="block">{content}</span>
+    </div>
+  </li>
+{:else if sender == 'counsellor'}
+  <li class="flex justify-end">
+    <div
+      class="relative max-w-xl px-4 py-2 text-gray-700 bg-gray-100 rounded shadow"
+    >
+      <span class="block">{content}</span>
+    </div>
+  </li>
+{/if}
---
+++ frontend/src/routes/learning-outcomes/[moduleName].svelte
@@ -0,0 +1,113 @@
+<script context="module">
+  export async function load({ params }) {
+    return { props: { moduleName: params.moduleName } };
+  }
+</script>
+
+<script lang="ts">
+  export let moduleName: string;
+
+  function runChatbot() {
+    location.href = '/chatbot-simulation/' + moduleName;
+  }
+</script>
+
+<html lang="en" data-theme="cupcake" />
+
+<!-- This example requires Tailwind CSS v2.0+ -->
+<div class="py-12 my-12 mx-52 border-solid border border-gray-300">
+  <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <div class="lg:text-left">
+      <h2
+        class="text-base font-semibold uppercase tracking-wide text-purple-700"
+      >
+        {moduleName}: Module Title
+      </h2>
+      <p
+        class="mt-2 text-3xl font-extrabold leading-8 tracking-tight text-gray-900 sm:text-4xl"
+      >
+        Learning Outcomes
+      </p>
+      <p class="text-l mt-4 max-w-2xl text-gray-500 ">
+        <b>Description: </b>Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Mauris vehicula eros ac sapien mattis lobortis. Interdum et
+        malesuada fames ac ante ipsum primis in faucibus. Morbi suscipit, libero
+        a varius placerat, dolor dolor placerat enim, ut rhoncus lorem metus ut
+        justo. Fusce sollicitudin tristique urna, quis malesuada leo.
+      </p>
+    </div>
+
+    <div class="mt-10">
+      <dl class="space-y-10 md:grid md:gap-x-8 md:gap-y-10 md:space-y-0">
+        <div class="relative">
+          <dt>
+            <div
+              class="absolute flex h-12 w-12 items-center justify-center rounded-md text-white bg-purple-700"
+            >
+              <!-- Heroicon name: outline/globe-alt -->
+              <svg
+                class="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
+                />
+              </svg>
+            </div>
+            <p class="ml-16 text-lg font-medium leading-6 text-gray-900">
+              Outcome 1
+            </p>
+          </dt>
+          <dd class="mt-2 ml-16 max-w-2xl text-base text-gray-500">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores
+            impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis
+            ratione.
+          </dd>
+        </div>
+
+        <div class="relative">
+          <dt>
+            <div
+              class="absolute flex h-12 w-12 items-center justify-center rounded-md text-white bg-purple-700"
+            >
+              <!-- Heroicon name: outline/scale -->
+              <svg
+                class="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"
+                />
+              </svg>
+            </div>
+            <p class="ml-16 text-lg font-medium leading-6 text-gray-900">
+              Outcome 2
+            </p>
+          </dt>
+          <dd class="mt-2 ml-16 max-w-2xl text-base text-gray-500">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores
+            impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis
+            ratione.
+          </dd>
+        </div>
+      </dl>
+    </div>
+  </div>
+  <button on:click={runChatbot} class="btn btn-primary btn-outline ml-10 mt-10">
+    Start Chatbot Simulation
+  </button>
+</div>
