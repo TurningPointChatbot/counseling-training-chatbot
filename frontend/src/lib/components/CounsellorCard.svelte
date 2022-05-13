b23afc33f13cf5c236f1864fa97052074309d0d5
mgam0010 <mgam0010@student.monash.edu>
2022-05-13 15:49:20 +1000

    Added employee view page

---
+++ frontend/src/lib/components/CounsellorCard.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+
+    
+    export let employeeData: any;
+
+    let employee = employeeData;
+
+  </script>
+
+  <div class="card card-side bg-base-100 shadow-xl">
+    <figure><img
+        alt={employee.name}
+        class="rounded-rectangle"
+        src={employee.profile_image}
+      /></figure>
+    <div class="employee-card card-body text-primary">
+        <h5 class="card-title">{employee.name}</h5>
+        <div class="card p-3">
+            <div class="flex flew-row">
+              <div class=" employee-description flex items-center basis-3/4">
+                <div>
+                    <div class="row employee-role">
+                        Position: {employee.position}
+                    </div>
+
+                    <div class="row employee-tenure">
+                        Tenure: {employee.tenure}
+                    </div>
+
+                    <div class="row employee-training">
+                        Total training {employee.training} hours
+                    </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  
+  <style>
+      
+    .rounded-rectangle {
+      /* Crops the image to a rectangle with rounded corners. */
+      object-fit: cover;
+      border-radius: 25px;
+    }
+    .card-title {
+      font-size: 24px;
+    }
+  
+    .employee-role {
+      font-size: 16px;
+      color: gray;
+    }
+    .employee-description{
+        padding-left: 10px;
+    }
+    .employee-card{
+        width: 580px;
+    }
+  
+  </style>
---
+++ frontend/src/routes/admin/employee.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import AdminBar from '$lib/components/admin/AdminBar.svelte';
+    import ModuleBigCard from '$lib/components/moduleBigCard.svelte';
+    import CounsellorCard from '$lib/components/counsellorCard.svelte';
+    import ModuleOverallProgress from '$lib/components/moduleOverallProgress.svelte';
+
+    let employee =
+    {
+      name: 'Davos Sand',
+      progress: 50,
+      profile_image: 'https://picsum.photos/id/426/400/600.jpg',
+      position: "Junior Counsellor",
+      tenure: "6 months",
+      training: "63"
+    }; 
+  </script>
+  
+
+<AdminBar>
+    <h1 class="m-8">Dashboard</h1>
+    <div class="grid grid-cols-2 gap-4 m-8">
+      <div class="..."><CounsellorCard employeeData={employee}/></div>
+      <div class="..."><ModuleOverallProgress /></div>
+      <div class="col-span-2 ..."><ModuleBigCard /></div>
+    </div>
+</AdminBar>  
+  
