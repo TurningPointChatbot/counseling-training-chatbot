f85c135bb3c3a6f480b2e2d686388caecbdb2704
Jason Tay <jtay0034@student.monash.edu>
2022-05-15 22:17:37 +1000

    made an async function in employee for retrieving number of completed modules

--- frontend/src/routes/admin/employee.svelte
+++ frontend/src/routes/admin/employee.svelte
@@ -3,6 +3,7 @@ import AdminBar from '$lib/components/admin/AdminBar.svelte';
 import ModuleBigCard from '$lib/components/dashboard/ModuleBigCard.svelte';
 import CounsellorCard from '$lib/components/dashboard/CounsellorCard.svelte';
 import ModuleOverallProgress from '$lib/components/dashboard/ModuleOverallProgress.svelte';
+import supabase from '$lib/supabase';
 
 let employee = {
   name: 'Davos Sand',
@@ -12,13 +13,40 @@ let employee = {
   tenure: "6 months",
   training: "63"
 };
+
+let totalCompleted = 0;
+let totalModules = 1;
+
+async function loadInEmployeeData() {
+  try {
+    let id = "";
+
+    let {data, error} = await supabase.from('chatbot_assignment').select().eq('user_id', id);
+
+    if (data) {
+      totalModules = data.length;
+
+      let modulesDone = 0;
+      for (let i = 0; i < data.length; i++) {
+        if (data[i].completed) {
+          modulesDone += 1;
+        }
+      }
+
+      totalCompleted = modulesDone;
+    }
+  } catch (error) {
+    alert(error.message);
+  }
+}
+
 </script>
 
 <AdminBar>
   <h1 class="m-8">Dashboard</h1>
   <div class="grid grid-cols-2 gap-4 m-8">
     <div class="..."><CounsellorCard employeeData={employee} /></div>
-    <div class="..."><ModuleOverallProgress /></div>
+    <div class="..."><ModuleOverallProgress totalModules={totalModules} completedModules={totalCompleted}/></div>
     <div class="col-span-2 ..."><ModuleBigCard /></div>
   </div>
 </AdminBar>
