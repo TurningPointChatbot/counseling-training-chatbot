b6332d1b041269923ba5452386e569f7a6df1d38
Jason Tay <jtay0034@student.monash.edu>
2022-05-15 22:17:37 +1000

    made an async function in employee for retrieving number of completed modules

--- frontend/src/lib/components/dashboard/ModuleOverallProgress.svelte
+++ frontend/src/lib/components/dashboard/ModuleOverallProgress.svelte
@@ -2,7 +2,8 @@
   import TimeRemaining from './TimeRemaining.svelte';
   import ModProgress from './ModuleProgress.svelte';
 
-  //export let modules = null;
+  export let totalModules = 1;
+  export let completedModules = 0;
 </script>
 
 <div class="card bg-base-100 shadow-xl">
@@ -14,7 +15,7 @@
   <hr class="bg-base-400" />
   <div class="card-body grid grid-row gap-3 w-full">
     <div class="card-body grid grid-cols-20 gap-3 w-full">
-      <div class="row"><ModProgress /></div>
+      <div class="row"><ModProgress total={totalModules} completed={completedModules}/></div>
     </div>
     <div class="card-body grid grid-cols-20 gap-3 w-full">
       <div class="row"><TimeRemaining /></div>
