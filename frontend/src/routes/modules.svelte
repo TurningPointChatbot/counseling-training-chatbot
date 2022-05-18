ad182029d3adce828d708be2f085511e01f035c9
Brent <bdia0003@student.monash.edu>
2022-05-18 20:18:43 +1000

    feat: added nav bar to module page

--- frontend/src/routes/modules.svelte
+++ frontend/src/routes/modules.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import AdminBar from '$lib/components/admin/AdminBar.svelte';
   import ModuleRow from '../lib/components/ModuleRow.svelte';
 
   // Array containing array of module values [[moduleName, dueDate, progress, description], ...]
@@ -21,7 +22,7 @@
 </script>
 
 <html lang="en" data-theme="cupcake" />
-
+<AdminBar>
 <!--Header-->
 <div class="grid-flow-col gap-8 my-4">
   <div
@@ -42,3 +43,4 @@
 {#each modules as module}
   <ModuleRow {...module} />
 {/each}
+</AdminBar>
