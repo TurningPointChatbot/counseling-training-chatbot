1dc08eaf1236fea69e51befd7c85f684b1568255
Linton Charles <lintoncharles2000@gmail.com>
2022-05-18 19:38:54 +1000

    feat: auto scrolling to bottom

--- frontend/src/lib/components/ChatMessage.svelte
+++ frontend/src/lib/components/ChatMessage.svelte
@@ -1,20 +1,28 @@
 <script lang="ts">
+import { onMount } from "svelte";
+
+
   export let sender: string;
   export let content: string;
+
+  let messageElement;
+  onMount(() => messageElement.scrollIntoView());
 </script>
 
-{#if sender == 'patient'}
-  <li class="flex justify-start">
-    <div class="relative max-w-xl px-4 py-2 text-gray-700 bg-base-400 rounded shadow">
-      <span class="block">{content}</span>
-    </div>
-  </li>
-{:else if sender == 'counsellor'}
-  <li class="flex justify-end">
-    <div
-      class="relative max-w-xl px-4 py-2 text-gray-700 bg-blue-light rounded shadow"
-    >
-      <span class="block">{content}</span>
-    </div>
-  </li>
-{/if}
+<div bind:this={messageElement}>
+  {#if sender == 'patient'}
+    <li class="flex justify-start">
+      <div class="relative max-w-xl px-4 py-2 text-gray-700 bg-base-400 rounded shadow">
+        <span class="block">{content}</span>
+      </div>
+    </li>
+  {:else if sender == 'counsellor'}
+    <li class="flex justify-end">
+      <div
+        class="relative max-w-xl px-4 py-2 text-gray-700 bg-blue-light rounded shadow"
+      >
+        <span class="block">{content}</span>
+      </div>
+    </li>
+  {/if}
+</div>
