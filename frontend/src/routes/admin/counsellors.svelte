395199b1a0e4312b4a676d6cd4157f35cf6b7f78
Jason Tay <jtay0034@student.monash.edu>
2022-05-19 18:45:12 +1000

    counesellors now pass their user id to href link

--- frontend/src/routes/admin/counsellors.svelte
+++ frontend/src/routes/admin/counsellors.svelte
@@ -8,37 +8,37 @@
       title: 'Davos Sand',
       description: 'Junior Counsellor',
       image: 'https://picsum.photos/id/426/400/600.jpg',
-      href: '/admin/employee'
+      href: '/admin/employee-details/id'
     },
     {
       title: 'Evelyn Chua',
       description: 'Junior Counsellor',
       image: 'https://picsum.photos/id/426/400/600.jpg',
-      href: '/admin/employee'
+      href: '/admin/employee-details/id'
     },
     {
       title: 'Isabella Howard',
       description: 'Junior Counsellor',
       image: 'https://picsum.photos/id/426/400/600.jpg',
-      href: '/admin/employee'
+      href: '/admin/employee-details/id'
     },
     {
       title: 'Jackson Tyler',
       description: 'Junior Counsellor',
       image: 'https://picsum.photos/id/426/400/600.jpg',
-      href: '/admin/employee'
+      href: '/admin/employee-details/id'
     },
     {
       title: 'Carey Smith',
       description: 'Junior Counsellor',
       image: 'https://picsum.photos/id/426/400/600.jpg',
-      href: '/admin/employee'
+      href: '/admin/employee-details/id'
     },
     {
       title: 'Joanna Tran',
       description: 'Junior Counsellor',
       image: 'https://picsum.photos/id/426/400/600.jpg',
-      href: '/admin/employee'
+      href: '/admin/employee-details/id'
     }
   ];
 </script>
