e34f0210e47b5184560482e563aaa8a1f5334ad3
Anna <anna.giang1999@gmail.com>
2022-05-19 16:14:06 +1000

    fix: move the login/logout icon to the right of the navbar
    Also fix code style in AdminBar.svelte.

--- frontend/src/lib/components/admin/AdminBar.svelte
+++ frontend/src/lib/components/admin/AdminBar.svelte
@@ -1,8 +1,9 @@
 <div class="drawer">
   <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+
   <div class="drawer-content">
     <div class="navbar bg-primary">
-      <div class="flex-none">
+      <div class="flex-1">
         <label for="my-drawer-3" class="btn btn-square btn-ghost">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -14,40 +15,30 @@
               stroke-linejoin="round"
               stroke-width="2"
               d="M4 6h16M4 12h16M4 18h16"
-            /></svg
-          >
+            /></svg>
         </label>
       </div>
-      <div class="dropdown">
-        <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-          <div class="w-10 rounded-full">
-            <img
-              src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
-            />
-          </div>
-        </label>
-        <ul
-          tabindex="0"
-          class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
-        >
-          <li>
-            <a
-              ><div>
-                Administrator<br />
-                <b>Name</b>
-              </div></a
-            >
-          </li>
-          <li><a>Logout</a></li>
-        </ul>
+      <div class="flex-none gap-2">
+        <div class="dropdown dropdown-end">
+          <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+            <div class="w-10 rounded-full">
+              <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" />
+            </div>
+          </label>
+          <ul tabindex="0" class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+            <li>
+              <a><div>Administrator<br /><b>Name</b></div></a>
+            </li>
+            <li><a>Logout</a></li>
+          </ul>
+        </div>
       </div>
     </div>
-
     <div class="px-16 h-full">
       <slot />
     </div>
-    
   </div>
+
   <div class="drawer-side">
     <label for="my-drawer-3" class="drawer-overlay" />
     <ul class="menu p-4 overflow-y-auto w-80 bg-base-100">
@@ -55,14 +46,10 @@
         <a href="/admin/dashboard" rel="prefetch" class="nav-link">Dashboard</a>
       </li>
       <li>
-        <a href="/admin/counsellors" rel="prefetch" class="nav-link"
-          >Counsellors</a
-        >
+        <a href="/admin/counsellors" rel="prefetch" class="nav-link">Counsellors</a>
       </li>
       <li>
-        <a href="/admin/modules" rel="prefetch" class="nav-link"
-          >Training Modules</a
-        >
+        <a href="/admin/modules" rel="prefetch" class="nav-link">Training Modules</a>
       </li>
     </ul>
   </div>
