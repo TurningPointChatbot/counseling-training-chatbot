8f7a1cf4f449a7d096023915690f42e33c64bc01
Harpo Barrett <hbar0009@student.monash.edu>
2022-05-19 15:58:27 +1000

    chore: run prettier

--- frontend/src/lib/components/dashboard/ModuleCard.svelte
+++ frontend/src/lib/components/dashboard/ModuleCard.svelte
@@ -4,7 +4,7 @@
 
 <a
   class="p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center"
-  href="/" 
+  href="/"
 >
   <!-- TODO: Correct url -->
   <img
