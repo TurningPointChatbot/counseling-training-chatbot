b9f129a9dd644ffadc27a369f90203280f5855ba
Harpo Barrett <hbar0009@student.monash.edu>
2022-05-23 20:35:13 +1000

    feat: add redirection to login and modules page from homepage based on login state

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,3 +1,20 @@
+<script context="module" lang="ts">
+  import { user } from '$lib/userStore';
+
+  /** @type {import('@sveltejs/kit').Load} */ export function load() {
+    let redirect_url: string = '/login';
+
+    if (user) {
+      redirect_url = 'modules';
+    }
+
+    return {
+      status: 302,
+      redirect: redirect_url
+    };
+  }
+</script>
+
 <script lang="ts">
   import ModulePage from './modules.svelte';
 </script>
