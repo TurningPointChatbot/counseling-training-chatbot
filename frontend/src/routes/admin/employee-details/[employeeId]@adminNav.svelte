dd9810eb36d75b1fd450ac0f0bccb2c109dded7d
Jason Tay <jtay0034@student.monash.edu>
2022-07-29 18:42:30 +1000

    Did some reformatting to match figma

--- frontend/src/routes/admin/employee-details/[employeeId]@adminNav.svelte
+++ frontend/src/routes/admin/employee-details/[employeeId]@adminNav.svelte
@@ -20,19 +20,47 @@ let employee = {
   position: "Junior Counsellor"
 };
 
-let totalCompleted = 0;
-let totalModules = 1;
-let totalHoursDone = 0;
+let totalCompleted = 1;
+let totalModules = 2;
+let totalHoursDone = 10;
 let targetTrainingHours = 100;
+
+async function loadInEmployee() {
+  try {
+    let {data, error} = await supabase.from('user').select().eq('id', employeeId);
+    if (data) {
+        employee.name = data[0].fname + ' ' + data[0].lname;
+        employee.profile_image = data[0].avatar_url;
+        employee.position = await getUserType(data[0].type_id)
+    }
+  } catch (error) {
+    alert(error.message);
+  }
+}
+
+async function getUserType(typeId) {
+  try {
+    let {data, error} = await supabase.from('user_type').select().eq('id', typeId);
+    if (data) {
+      return data[0].name
+    } else {
+      return '';
+    }
+  } catch(error) {
+    return '';
+  }
+}
 
 async function getModulesData() {
   try {
 
+    // Select all modules assigned to this employee
     let {data, error} = await supabase.from('chatbot_assignment').select().eq('user_id', employeeId);
 
     if (data) {
       totalModules = data.length;
 
+      // Count the number of modules done
       let modulesDone = 0;
       for (let i = 0; i < data.length; i++) {
         if (data[i].completed) {
@@ -50,6 +78,7 @@ async function getModulesData() {
 async function getTrainingHours() {
   try {
 
+    // Get all chatbot attempts from this employee to count his training hours from the attempts.
     let {data, error} = await supabase.from('chatbot_attempt').select().eq('user_id', employeeId);
 
     if (data) {
@@ -73,14 +102,15 @@ function calculateHours(startTimeStamp: string, completedTimeStamp: string) {
   return timeDiff / (1000 * 60 * 60);
 }
 
+loadInEmployee();
 getModulesData();
 getTrainingHours();
 
 </script>
 
-  <h1 class="m-8">Dashboard</h1>
+  <h1 class="m-8">{employee.name}</h1>
   <div class="grid grid-cols-2 gap-4 m-8">
-    <div class="..."><CounsellorCard employeeData={employee} /></div>
+    <div class="..."><CounsellorCard employeeData={employee} {totalHoursDone}/></div>
     <div class="..."><ModuleOverallProgress {totalModules} completedModules={totalCompleted}
       completedHours={totalHoursDone} targetHours={targetTrainingHours}/></div>
     <div class="col-span-2 ..."><ModuleBigCard /></div>
