02f700bb3707331aab68d182bd113eb05490bfb3
Brent <bdia0003@student.monash.edu>
2022-08-06 18:10:39 +1000

    Refactor: updated module list for counsellors. Module list now looks like that on admin page.

--- frontend/src/routes/admin/modules.svelte
+++ frontend/src/routes/admin/modules.svelte
@@ -1,36 +1,74 @@
-<script>
-  import AdminBar from '$lib/components/admin/AdminBar.svelte';
+<script lang="ts">
   import FilterableList from '$lib/components/FilterableList.svelte';
+  import supabase from '$lib/supabase';
+  
+  
+  async function getModulesList() {
+    let api_modules = [];
+    try {
+      let {data, error} = await supabase
+        .from('admin_module')
+        .select();
+  
+      if (data) {
+        //console.log(data);
+        for (let i = 0; i < data.length; i++) {
+          let module = {title: data[i].title, description: data[i].description, image: 'https://picsum.photos/id/426/400/600.jpg', dateAccessed: data[i].dateAccessed};
+          //console.log(module);
+          api_modules.push(module);
+          //console.log(api_modules);
+        }
+      }
+      if (error) throw error;
+    } catch (error) {
+      alert(error.message);
+    }
+    return api_modules;
+  }
+  let promise = getModulesList()
+
+
+
+  
 
   // TODO: Retrieve module details from database.
-  let modules = [
+  /*let modules = [
     {
       title: 'Lesson 101: Module Title',
       description:
         'Short summary lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vulputate ipsum vel nisi aliquam euismod.',
-      image: 'https://picsum.photos/id/426/400/600.jpg'
+      image: 'https://picsum.photos/id/426/400/600.jpg',
+      href: '/admin/module-details/moduleId'
     },
     {
       title: 'Tutorial 102: Module Title',
       description:
         'Short summary lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vulputate ipsum vel nisi aliquam euismod.',
-      image: 'https://picsum.photos/id/426/400/600.jpg'
+      image: 'https://picsum.photos/id/426/400/600.jpg',
+      href: '/admin/module-details/moduleId'
     },
     {
       title: 'Class 103: Module Title',
       description:
         'Short summary lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vulputate ipsum vel nisi aliquam euismod.',
-      image: 'https://picsum.photos/id/426/400/600.jpg'
+      image: 'https://picsum.photos/id/426/400/600.jpg',
+      href: '/admin/module-details/moduleId'
     },
     {
       title: 'Module 104: Module Title',
       description:
         'Short summary lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vulputate ipsum vel nisi aliquam euismod.',
-      image: 'https://picsum.photos/id/426/400/600.jpg'
+      image: 'https://picsum.photos/id/426/400/600.jpg',
+      href: '/admin/module-details/moduleId'
     }
-  ];
+  ];*/
 </script>
 
-<AdminBar>
-  <FilterableList listData={modules} />
-</AdminBar>
+  <div class="m-3">
+    <h1>Modules</h1>
+  </div>
+  <div class="m-3 h-3/4">
+    {#await promise then modules}
+      <FilterableList listData={modules} rectangleOrCircle={true}/>
+    {/await}
+  </div>
--- frontend/src/routes/modules.svelte
+++ frontend/src/routes/modules.svelte
@@ -4,6 +4,7 @@
   import supabase from '$lib/supabase';
   import userStore from '../stores/authStore';
   import type { Session, User } from '@supabase/supabase-js';
+  import FilterableList from '$lib/components/FilterableList.svelte';
 
   interface Module {
     moduleName: string;
@@ -14,6 +15,24 @@
   let session: Session;
   let user: User;
   let userModules: Array<Module> = [];
+  let modules: Array<any> = [];
+
+  /*
+  for (let i: number = 1; i <= 8; i++) {
+    let moduleName: string = 'Module ' + String(i);
+    let dueDate: string = String(i) + '/06/22';
+    let progress: number = (i + 2) * 10;
+    let description: string =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque in dui non volutpat. Suspendisse non turpis eget purus viverra sollicitudin. Morbi nulla nisi, consequat non neque vel, scelerisque porta orci. Aenean vitae neque imperdiet tellus cursus ornare. Etiam dignissim pellentesque fermentum. Etiam feugiat nibh vel mattis eleifend. Praesent orci.';
+    modules.push({
+      title: moduleName,
+      dueDate: dueDate,
+      progress: progress,
+      image: 'https://picsum.photos/id/426/400/600.jpg',
+      description: description
+    });
+  }
+  */
 
   async function getModulesForUser(user) {
     console.log(user);
@@ -49,7 +68,8 @@
             dueDate: assignments[i].duedate,
             progress: (i + 1 + 2) * 10,
             description: data.chatbot_module[i].description
-          });
+          }
+          )
         }
       }
       userModules = userModules;
@@ -74,11 +94,61 @@
   (async () => {
     await getModulesForUser(user);
   })();
+
+  async function getModulesList(user) {
+    modules = [];
+    try {
+      let { data, error } = await supabase
+        .from('user')
+        .select(
+          ` 
+        id,
+        email,
+        chatbot_assignment(
+          user_id,
+          duedate,
+          created_at,
+          completed_at,
+          completed
+        ),
+        chatbot_module!chatbot_assignment(
+          title, 
+          description
+        )
+        `
+        )
+        .eq('email', user.email)
+        .single();
+      if (error) throw error;
+      if (data) {
+        console.log(data);
+        let assignments = data.chatbot_assignment;
+        for (let i: number = 0; i < assignments.length; i++) {
+          
+          modules.push({
+            title: 'Module ' + String(i + 1),
+            description: data.chatbot_module[i].description, 
+            image: 'https://picsum.photos/id/426/400/600.jpg',
+            dateAccessed: '2022-05-25'});
+            
+          console.log(modules);
+        }
+      }
+      if (error) throw error;
+    } catch (error) {
+      alert(error.message);
+    }
+    return modules;
+  }
+
+  let promise = getModulesList(user);
+
 </script>
 
 <html lang="en" data-theme="cupcake" />
 <CounsellorBar>
-  <!--Header-->
+  <!--
+
   <div class="grid-flow-col gap-8 my-4">
     <div
       tabindex="0"
@@ -95,7 +165,20 @@
     </div>
   </div>
 
+  
   {#each userModules as module}
     <ModuleRow {...module} />
   {/each}
+  -->
+
+
+
+<div class="m-3">
+  <h1>Modules</h1>
+</div>
+<div class="m-3 h-3/4">
+  {#await promise then modules}
+  <FilterableList listData={modules} rectangleOrCircle={true}/>
+  {/await}
+</div>
 </CounsellorBar>
