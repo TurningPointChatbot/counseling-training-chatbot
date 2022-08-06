9eb267ca1f45c5b46755f6cc4b3c9454b704ff2a
Anna <anna.giang1999@gmail.com>
2022-08-06 14:38:26 +1000

    test: tests for AdminSidenav and CounsellorSidenav components
    Also fixed the code formatting in some files.

--- frontend/src/routes/__layout.svelte
+++ frontend/src/routes/__layout.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-  import { session } from '$app/stores';
+  import { session, page } from '$app/stores';
   import supabaseClient from '$lib/supabase';
   import { SupaAuthHelper } from '@supabase/auth-helpers-svelte';
   import '../app.css';
-  import { page } from '$app/stores'
   import supabase from '$lib/supabase';
   import AdminSidenav from '$lib/components/sidenav/AdminSidenav.svelte';
   import CounsellorSidenav from '$lib/components/sidenav/CounsellorSidenav.svelte';
@@ -13,7 +12,7 @@
 
   // TODO: All the below JavaScript code is copied from Profile.svelte and Login.svelte - these common functions
   // should ideally be refactored out into a single place.
-  
+
   /**
    * Sign Out Function
    */
@@ -33,7 +32,7 @@
   /**
    * Retrieves a profile picture's URL and sets it to src.
    */
-   async function downloadImage(path) {
+  async function downloadImage(path) {
     try {
       const { data, error } = await supabase.storage
         .from('avatars')
@@ -49,7 +48,7 @@
   }
 
   /**
-   * Retrieve user information from the database and update UI 
+   * Retrieve user information from the database and update UI
    */
   async function getUserInfo() {
     if ($page.url.pathname === '/login') {
@@ -96,29 +95,46 @@
     </SupaAuthHelper>
   </div>
 {:else}
-  <div class='drawer'>
-    <input id='my-drawer' type='checkbox' class='drawer-toggle' />
-    <div class='drawer-content'>
-
-      <div class='navbar bg-primary'>
-        <div class='flex-none'>
-          <label for='my-drawer' class='btn btn-square btn-ghost drawer-button'>
-            <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' class='inline-block w-5 h-5 stroke-white'><path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M4 6h16M4 12h16M4 18h16'></path></svg>
+  <div class="drawer">
+    <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content">
+      <div class="navbar bg-primary">
+        <div class="flex-none">
+          <label for="my-drawer" class="btn btn-square btn-ghost drawer-button">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="inline-block w-5 h-5 stroke-white"
+              ><path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              /></svg
+            >
           </label>
         </div>
-        <div class='flex-1 pb-1'>
-          <img class='max-h-12' alt='Turning point logo' src='/turning-point-logo.svg' />
+        <div class="flex-1 pb-1">
+          <img
+            class="max-h-12"
+            alt="Turning point logo"
+            src="/turning-point-logo.svg"
+          />
         </div>
-        <div class='flex-none'>
-          <div class='dropdown dropdown-end'>
-            <button tabindex='0' class='btn btn-ghost btn-circle avatar'>
-              <div class='w-10 rounded-full'>
-                <img alt='profile' src={avatarUrl} />
+        <div class="flex-none">
+          <div class="dropdown dropdown-end">
+            <button tabindex="0" class="btn btn-ghost btn-circle avatar">
+              <div class="w-10 rounded-full">
+                <img alt="profile" src={avatarUrl} />
               </div>
             </button>
-            <ul tabindex='0' class='mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52'>
+            <ul
+              tabindex="0"
+              class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+            >
               <li><a href={'/profile/account'}>Profile</a></li>
-              <li on:click={signOut}><a href='../login'>Logout</a></li>
+              <li on:click={signOut}><a href="../login">Logout</a></li>
             </ul>
           </div>
         </div>
@@ -128,15 +144,15 @@
           <slot/>
         </SupaAuthHelper>
       </div>
-    </div> 
-    <div class='drawer-side'>
-      <label for='my-drawer' class='drawer-overlay'></label>
-      <ul class='menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content'>
+    </div>
+    <div class="drawer-side">
+      <label for="my-drawer" class="drawer-overlay" />
+      <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
         {#if userType === 'admin' || userType === 'supervisor'}
           <AdminSidenav />
         {/if}
 
-        {#if userType === 'counsellor' }
+        {#if userType === 'counsellor'}
           <CounsellorSidenav />
         {/if}
       </ul>
