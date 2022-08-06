7272e7081a7aa4e0057b6bbc22ef27675a436f67
Anna <anna.giang1999@gmail.com>
2022-08-06 14:38:26 +1000

    test: tests for AdminSidenav and CounsellorSidenav components
    Also fixed the code formatting in some files.

--- frontend/src/lib/components/sidenav/AdminSidenav.svelte
+++ frontend/src/lib/components/sidenav/AdminSidenav.svelte
@@ -1,7 +1,7 @@
 <script>
-  import { AdminRoutes } from '$lib/constants.ts'
+  import { AdminRoutes } from '$lib/constants.ts';
 </script>
 
-<li><a href={ AdminRoutes.Dashboard }>Dashboard</a></li>
-<li><a href={ AdminRoutes.Counsellors }>Counsellors</a></li>
-<li><a href={ AdminRoutes.Modules }>Training Modules</a></li>
+<li><a href={AdminRoutes.Dashboard}>Dashboard</a></li>
+<li><a href={AdminRoutes.Counsellors}>Counsellors</a></li>
+<li><a href={AdminRoutes.Modules}>Training Modules</a></li>
