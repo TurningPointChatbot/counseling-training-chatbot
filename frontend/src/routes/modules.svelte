0b437c7397fb4d8e5d0a8e03ab0ff92f8e1e7a74
Brent <bdia0003@student.monash.edu>
2022-08-12 12:17:50 +1000

    Refactor: Removed unused functions and code. Added documentation to the new functions that are used to match UI.

--- frontend/src/routes/modules.svelte
+++ frontend/src/routes/modules.svelte
@@ -4,7 +4,6 @@
   import supabase from '$lib/supabase';
   import userStore from '../stores/authStore';
   import type { Session, User } from '@supabase/supabase-js';
-  import FilterableList from '$lib/components/FilterableList.svelte';
 
   interface Module {
     moduleName: string;
@@ -14,70 +13,7 @@
   }
   let session: Session;
   let user: User;
-  let userModules: Array<Module> = [];
   let modules: Array<any> = [];
-
-  /*
-  for (let i: number = 1; i <= 8; i++) {
-    let moduleName: string = 'Module ' + String(i);
-    let dueDate: string = String(i) + '/06/22';
-    let progress: number = (i + 2) * 10;
-    let description: string =
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque in dui non volutpat. Suspendisse non turpis eget purus viverra sollicitudin. Morbi nulla nisi, consequat non neque vel, scelerisque porta orci. Aenean vitae neque imperdiet tellus cursus ornare. Etiam dignissim pellentesque fermentum. Etiam feugiat nibh vel mattis eleifend. Praesent orci.';
-    modules.push({
-      title: moduleName,
-      dueDate: dueDate,
-      progress: progress,
-      image: 'https://picsum.photos/id/426/400/600.jpg',
-      description: description
-    });
-  }
-  */
-
-  async function getModulesForUser(user) {
-    console.log(user);
-    try {
-      let { data, error } = await supabase
-        .from('user')
-        .select(
-          ` 
-        id,
-        email,
-        chatbot_assignment(
-          user_id,
-          duedate,
-          created_at,
-          completed_at,
-          completed
-        ),
-        chatbot_module!chatbot_assignment(
-          title, 
-          description
-        )
-        `
-        )
-        .eq('email', user.email)
-        .single();
-      if (error) throw error;
-      if (data) {
-        console.log(data);
-        let assignments = data.chatbot_assignment;
-        for (let i: number = 0; i < assignments.length; i++) {
-          userModules.push({
-            moduleName: 'Module ' + String(i + 1),
-            dueDate: assignments[i].duedate,
-            progress: (i + 1 + 2) * 10,
-            description: data.chatbot_module[i].description
-          }
-          )
-        }
-      }
-      userModules = userModules;
-    } catch (error) {
-      console.log(error.hint);
-      console.log(error.details);
-    }
-  }
 
   userStore.subscribe((value) => {
     // session
@@ -91,10 +27,10 @@
     console.log(user);
   });
 
-  (async () => {
-    await getModulesForUser(user);
-  })();
-
+  /**
+   * Getting a list of assigned modules for the user signed in.
+   * @param user
+   */
   async function getModulesList(user) {
     modules = [];
     try {
@@ -121,10 +57,10 @@
         .single();
       if (error) throw error;
       if (data) {
-        console.log(data);
-        let assignments = data.chatbot_assignment;
+        // all modules assigned
+        let assignments = data.chatbot_assignment; 
         for (let i: number = 0; i < assignments.length; i++) {
-          
+          // adding relevant info from database to the modules list locally.
           modules.push({
             title: 'Module ' + String(i + 1),
             description: data.chatbot_module[i].description, 
@@ -147,36 +83,11 @@
 
 <html lang="en" data-theme="cupcake" />
 <CounsellorBar>
-  <!--
-
-  <div class="grid-flow-col gap-8 my-4">
-    <div
-      tabindex="0"
-      class="collapse collapse-close border border-base-300 bg-base-100 rounded-box"
-      style="background-color:#852E82; color:white"
-    >
-      <div class="collapse-title text-2xl font-medium">
-        <div class="grid grid-cols-3 gaps-4">
-          <div>Module Name</div>
-          <div>Due Date</div>
-          <div>Status</div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  
-  {#each userModules as module}
-    <ModuleRow {...module} />
-  {/each}
-  -->
-
-
-
 <div class="m-3">
   <h1>Modules</h1>
 </div>
 <div class="m-3 h-3/4">
+  <!-- Generating the list of modules using the ModuleRow.svelte-->
   {#await promise then modules}
   <ModuleRow listData={modules} rectangleOrCircle={true}/>
   {/await}
