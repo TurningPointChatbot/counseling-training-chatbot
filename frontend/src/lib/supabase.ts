ff9bc0381bc291a2b4ca870d5c207cceaad49c6b
Jason Tay <jtay0034@student.monash.edu>
2022-08-12 14:39:24 +1000

    imported some modules in tsconfig

--- frontend/src/lib/supabase.ts
+++ frontend/src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createSupabaseClient } from '@supabase/auth-helpers-sveltekit';
 
-const { supabase } = createSupabaseClient(
+const supabase = createSupabaseClient(
   <string>import.meta.env.VITE_SUPABASE_URL,
   <string>import.meta.env.VITE_SUPABASE_ANON_KEY
 );
