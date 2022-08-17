a010162f6dc2ae0347e570096419c999f63f7b69
Harpo Barrett <hbar0009@student.monash.edu>
2022-08-17 20:15:15 +1000

    refactor: move toObject function into prisma.ts

--- frontend/src/lib/prisma.ts
+++ frontend/src/lib/prisma.ts
@@ -1,5 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+export const prisma = new PrismaClient();
+
+export function toObject(obj) {
+  return JSON.parse(
+    JSON.stringify(
+      obj,
+      (key, value) => (typeof value === 'bigint' ? value.toString() : value) // return everything else unchanged
+    )
+  );
+}
 
 export default prisma;
--- frontend/src/routes/api/users.json.ts
+++ frontend/src/routes/api/users.json.ts
@@ -1,17 +1,8 @@
-import prisma from '../../lib/prisma';
+import { prisma, toObject } from '../../lib/prisma';
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export async function GET() {
   const users = await prisma.user.findMany();
-
-  function toObject(obj) {
-    return JSON.parse(
-      JSON.stringify(
-        obj,
-        (key, value) => (typeof value === 'bigint' ? value.toString() : value) // return everything else unchanged
-      )
-    );
-  }
 
   if (users) {
     return {
