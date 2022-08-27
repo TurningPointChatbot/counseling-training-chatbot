ef9f51fe95cf70930d137bc12091ba1072b2a458
Katherine Moffat <kmof0001@student.monash.edu>
2022-08-27 15:47:13 +1000

    adding css styling tags for BigCard

--- frontend/src/lib/components/dashboard/BigCard.svelte
+++ frontend/src/lib/components/dashboard/BigCard.svelte
@@ -5,27 +5,11 @@
 </script>
 
 <div class="card-bordered m-3">
-  <div id="title" class="font-bold text-xl mb-2 text-gray-700 pt-3">
+  <div class="h1 left-4 top-3 relative">
     {title}
   </div>
-  <div id="subtitle">{subTitle}</div>
+  <div class="p-faded left-8 top-1 relative">{subTitle}</div>
   <div class="card-body grid grid-cols-7 gap-4 gap-2 py-2 grid-flow-col">
     <slot />
   </div>
 </div>
-
-<style>
-  #subtitle {
-    position: relative;
-    left: 30px;
-    top: 5px;
-    color: lightgrey;
-  }
-
-  #title {
-    position: relative;
-    left: 30px;
-    top: 10px;
-    letter-spacing: 0.025em;
-  }
-</style>
