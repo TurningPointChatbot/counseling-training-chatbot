2d71ce94dafdf709420f927eeb08a24c43153d24
Harpo Barrett <hbar0009@student.monash.edu>
2022-09-01 22:17:36 +1000

    feat: add basic get function for chatbot module endpoint

---
+++ frontend/src/routes/api/chatbot-modules/_api.ts
@@ -0,0 +1,33 @@
+import { chatbot_module } from '@prisma/client';
+import { prisma, toObject } from '../../../lib/prisma';
+
+type ChatbotModuleAPIGetParams = {
+  id?: number;
+  user_id?: number;
+};
+
+export async function chatbotModuleGET(
+  params?: ChatbotModuleAPIGetParams
+): Promise<chatbot_module[] | undefined> {
+  let modules: chatbot_module[] = [];
+
+  if (!params) {
+    modules = await prisma.chatbot_module.findMany();
+  } else {
+    const foundModule = await prisma.user.findUnique({
+      where: {
+        id: params.id
+      }
+    });
+
+    if (foundModule) {
+      modules = toObject(foundModule);
+    }
+  }
+
+  if (modules) {
+    return toObject(modules);
+  }
+
+  return;
+}
