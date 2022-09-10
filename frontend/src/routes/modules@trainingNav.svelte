636087abfec50f42c4061f05bfb215bbced83b1a
Brent <bdia0003@student.monash.edu>
2022-09-10 11:01:37 +1000

    Refactor: updated modules@trainingNav.svelte to connect to database with API.

--- frontend/src/routes/modules@trainingNav.svelte
+++ frontend/src/routes/modules@trainingNav.svelte
@@ -1,14 +1,50 @@
+<script context="module" lang="ts">
+
+  export async function load({ fetch}) {
+    const url = `api/chatbot-assignments/user_id=1`;
+    const response = await fetch(url);
+    console.log("hi");
+    return {
+      status: response.status,
+      props: {
+        assignments: response.ok && (await response.json())
+      }
+    };
+  }
+</script>
+
 <script lang="ts">
-  import CounsellorBar from '$lib/components/CounsellorBar.svelte';
   import ModuleRow from '../lib/components/ModuleRow.svelte';
   import supabase from '$lib/supabase';
   import userStore from '../stores/authStore';
   import type { Session, User } from '@supabase/supabase-js';
 
+  
+  import type { chatbot_assignment } from '@prisma/client';
+  export let assignments: Array<chatbot_assignment>;
+
   let session: Session;
   let user: User;
   let modules: Array<any>;
 
+  function createModuleList() {
+    modules = [];
+    for (let i: number = 0; i < assignments.length; i++) {
+      console.log(assignments.length);
+      modules.push({
+            title: "Module title of " + assignments[i].cbm_id, // should come from different table
+            description: "Module desc of " + assignments[i].cbm_id, // should come from different table
+            cbm_id: assignments[i].cbm_id, 
+            completed: assignments[i].completed,
+            image: 'https://picsum.photos/id/426/400/600.jpg', // should come from different table
+            dateAccessed: '2022-05-25'});
+      }
+    return modules;
+  }
+
+  modules = createModuleList();
+
+  /**
   async function getModulesList(user) {
     modules = [];
     try {
@@ -55,6 +91,9 @@
     }
     return modules;
   }
+  */
+
+  //let promise = getModulesList(user);
 
   userStore.subscribe((value) => {
     // session
@@ -68,8 +107,7 @@
     console.log(user);
   });
 
-  let promise = getModulesList(user);
-
+  
 </script>
 
 <html lang="en" data-theme="cupcake" />
@@ -77,9 +115,15 @@
     <h1>Modules</h1>
   </div>
   <div class="m-3 h-3/4">
-    <!-- Generating the list of modules using the ModuleRow.svelte-->
-    {#await promise then modules}
-      <ModuleRow listData={modules} rectangleOrCircle={true} />
-    {/await}
+    <!-- Generating the list of modules using the ModuleRow.svelte  --> 
+    <ModuleRow listData={modules} rectangleOrCircle={true} />
+     
+
   </div>
 
+<!-- UNCOMMENT THIS OUT IF YOU WANT TO SEE RETRIEVED JSON DATA 
+<h1>Json Data</h1>
+{#each modules as module}
+  <p>{JSON.stringify(module)}</p>
+{/each}
+-->
