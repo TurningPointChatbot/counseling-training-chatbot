9e6512a7bea3ac34b39727ca2c12ed98147d2ab1
Harpo Barrett <hbar0009@student.monash.edu>
2022-09-11 20:17:18 +1000

    fix: change bigint types in post_types to number

--- src/lib/post_types.ts
+++ src/lib/post_types.ts
@@ -1,10 +1,10 @@
 export type chatbot_attempt_POST = {
-  user_id: bigint;
-  cbm_id: bigint;
+  user_id: number;
+  cbm_id: number;
 };
 
 export type chatbot_attempt_message_POST = {
-  attempt_id: bigint;
+  attempt_id: number;
   text: string;
   message_type: 'user' | 'chatbot';
 };
