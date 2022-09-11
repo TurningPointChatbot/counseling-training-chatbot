aae896050ac858cc1a6c3526f6e9b07e45706566
Harpo Barrett <hbar0009@student.monash.edu>
2022-09-11 20:36:36 +1000

    refactor: modify message creation example to reflect new post types and route change, and add attempt creation example

--- src/routes/prisma-example/create-message-example.svelte
+++ src/routes/prisma-example/create-message-example.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
-  import { removeBigInt } from '$lib/helpers';
-  import type { chatbot_attempt_message_POST } from '$lib/post_types';
+  import type {
+    chatbot_attempt_message_POST,
+    chatbot_attempt_POST
+  } from '$lib/post_types';
 
   async function handleClick() {
     const creation_time = new Date();
@@ -11,13 +13,27 @@
       message_type: 'user'
     };
 
-    const result = await fetch('/api/chatbot_messages', {
+    const attempt: chatbot_attempt_POST = {
+      user_id: 1,
+      cbm_id: 1
+    };
+
+    const message_result = await fetch('/api/chatbot-messages', {
       method: 'POST',
-      body: JSON.stringify(removeBigInt(message))
+      body: JSON.stringify(message)
     });
 
-    console.log(result);
+    const attempt_result = await fetch('/api/chatbot-attempts', {
+      method: 'POST',
+      body: JSON.stringify(attempt)
+    });
+
+    console.log(await message_result.json());
+
+    console.log(await attempt_result.json());
   }
 </script>
 
-<button on:click={handleClick}> Click to send a new message </button>
+<button on:click={handleClick}>
+  Click to send a new message (and a new attempt!)
+</button>
