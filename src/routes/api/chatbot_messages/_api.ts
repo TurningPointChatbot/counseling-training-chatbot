13ef74b2bddc0fc4c2e935c7ea13018c8eb1b55a
Harpo Barrett <hbar0009@student.monash.edu>
2022-09-11 20:25:24 +1000

    refactor: change chatbot_messages POST api to use new post type

--- src/routes/api/chatbot_messages/_api.ts
+++ src/routes/api/chatbot_messages/_api.ts
@@ -1,16 +1,17 @@
 import { chatbot_attempt_message } from '@prisma/client';
 import { removeBigInt } from '../../../lib/helpers';
+import { chatbot_attempt_message_POST } from '../../../lib/post_types';
 import prisma from '../../../lib/prisma';
 
 export async function chatbotMessagesPOST(
-  new_message: chatbot_attempt_message
-) {
+  new_message: chatbot_attempt_message_POST
+): Promise<chatbot_attempt_message | undefined> {
   const created_message = await prisma.chatbot_attempt_message.create({
     data: {
       attempt_id: BigInt(new_message.attempt_id),
-      created_at: new_message.created_at,
+      created_at: new Date(),
       text: new_message.text,
-      message_type: BigInt(new_message.message_type)
+      message_type: BigInt(new_message.message_type === 'user' ? 1 : 2)
     }
   });
 
