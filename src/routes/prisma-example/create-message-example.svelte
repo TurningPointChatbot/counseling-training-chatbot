8cf16185ce8a315d24342aac9bc36d509d95b4b3
Harpo Barrett <61974761+hbar0009@users.noreply.github.com>
2022-09-11 20:43:03 +1000

    feat: chatbot attempts post (#44)
    * feat: add two new typescript types for POST endpoints
    
    * fix: change bigint types in post_types to number
    
    * feat: add POST endpoint at /api/chatbot-attempts/
    
    * refactor: change chatbot_messages POST api to use new post type
    
    * refactor: modify create message example to reflect new post type
    
    * chore: rename chatbot_messages endpoint to chatbot-messages for consistency
    
    * refactor: modify message creation example to reflect new post types and route change, and add attempt creation example

--- src/routes/prisma-example/create-message-example.svelte
+++ src/routes/prisma-example/create-message-example.svelte
@@ -1,24 +1,39 @@
 <script lang="ts">
-  import type { chatbot_attempt_message } from '@prisma/client';
-  import { removeBigInt } from '$lib/helpers';
+  import type {
+    chatbot_attempt_message_POST,
+    chatbot_attempt_POST
+  } from '$lib/post_types';
 
   async function handleClick() {
     const creation_time = new Date();
 
-    const message: chatbot_attempt_message = {
-      attempt_id: BigInt(2),
-      created_at: creation_time,
+    const message: chatbot_attempt_message_POST = {
+      attempt_id: 2,
       text: `New message at time ${creation_time.toLocaleTimeString()}`,
-      message_type: BigInt(1)
+      message_type: 'user'
     };
 
-    const result = await fetch('/api/chatbot_messages', {
+    const attempt: chatbot_attempt_POST = {
+      user_id: 1,
+      cbm_id: 1
+    };
+
+    const message_result = await fetch('/api/chatbot-messages', {
       method: 'POST',
-      body: JSON.stringify(removeBigInt(message))
+      body: JSON.stringify(message)
     });
 
-    console.log(result);
+    const attempt_result = await fetch('/api/chatbot-attempts', {
+      method: 'POST',
+      body: JSON.stringify(attempt)
+    });
+
+    console.log(await message_result.json());
+
+    console.log(await attempt_result.json());
   }
 </script>
 
-<button on:click={handleClick}> Click to send a new message </button>
+<button on:click={handleClick}>
+  Click to send a new message (and a new attempt!)
+</button>
