407c529ca413f7a29fa6a029670be1ba60e5b447
Harpo Barrett <hbar0009@student.monash.edu>
2022-09-11 20:21:16 +1000

    feat: add POST endpoint at /api/chatbot-attempts/

--- src/routes/api/chatbot-attempts/_api.ts
+++ src/routes/api/chatbot-attempts/_api.ts
@@ -1,4 +1,5 @@
 import { chatbot_attempt } from '@prisma/client';
+import { chatbot_attempt_POST } from '../../../lib/post_types';
 import { prisma } from '../../../lib/prisma';
 import { removeBigInt } from '../../../lib/helpers';
 
@@ -31,6 +32,27 @@ export async function chatbotAttemptsGET(
 
   if (attempts) {
     return removeBigInt(attempts);
+  }
+
+  return;
+}
+
+export async function chatbotAttemptsPOST(
+  new_attempt: chatbot_attempt_POST
+): Promise<chatbot_attempt | undefined> {
+  const current_time = new Date();
+
+  const created_attempt = await prisma.chatbot_attempt.create({
+    data: {
+      user_id: BigInt(new_attempt.user_id),
+      cbm_id: BigInt(new_attempt.cbm_id),
+      started_at: current_time,
+      last_accessed: current_time
+    }
+  });
+
+  if (created_attempt) {
+    return removeBigInt(created_attempt);
   }
 
   return;
