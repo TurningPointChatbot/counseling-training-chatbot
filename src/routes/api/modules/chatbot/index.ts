60f540276dd1d7c66f70ccaeb568e1790cb60a3e
Harpo Barrett <hbar0009@student.monash.edu>
2022-09-15 19:52:06 +1000

    feat: add POST endpoint at /api/modules/chatbot/

--- src/routes/api/modules/chatbot/index.ts
+++ src/routes/api/modules/chatbot/index.ts
@@ -1,4 +1,6 @@
-import { chatbotModuleGET } from './_api';
+import { chatbot_module } from '@prisma/client';
+import { chatbot_module_POST } from '../../../../lib/post_types';
+import { chatbotModuleGET, chatbotModulePOST } from './_api';
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export async function GET() {
@@ -14,5 +16,26 @@ export async function GET() {
 
   return {
     status: 404
+  };
+}
+
+/** @type {import('@sveltejs/kit').RequestHandler} */
+export async function POST({ request }) {
+  const new_module: chatbot_module_POST = await request.json();
+
+  const created_module: chatbot_module | undefined = await chatbotModulePOST(
+    new_module
+  );
+
+  if (created_module) {
+    return {
+      status: 200,
+      headers: {},
+      body: created_module
+    };
+  }
+
+  return {
+    status: 400
   };
 }
