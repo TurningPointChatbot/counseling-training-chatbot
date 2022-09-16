bdbe27e7687934b659e2aa1204e2f7d6c6066d1a
Linton Charles <lintoncharles2000@gmail.com>
2022-09-16 12:17:58 +1000

    fix: Nav links in chatbot pages

--- src/routes/chatbot-simulation/[moduleName].svelte
+++ src/routes/chatbot-simulation/[moduleName].svelte
@@ -98,7 +98,7 @@
           >Send Message</button
         >
         <button
-          on:click={() => (location.href = '/modules')}
+          on:click={() => (location.href = '/counsellor/modules')}
           type="button"
           class="btn-outline rounded inline-block px-6 py-2.5 bg-error text-white font-medium text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out float-right"
           >End Chat</button
