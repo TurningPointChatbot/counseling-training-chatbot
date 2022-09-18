0ac527ec62a3951bd38e6ad0e0d848c3d485d742
Jason Tay <jtay0034@student.monash.edu>
2022-09-18 13:53:17 +1000

    counsellors and modules now use prisma

--- src/routes/admin/counsellors.svelte
+++ src/routes/admin/counsellors.svelte
@@ -1,56 +1,39 @@
+<script context="module" lang="ts">
+  export async function load({ fetch }) {
+    const url = '/api/users/counsellors';
+    const response = await fetch(url);
+
+    return {
+      status: response.status,
+      props: {
+        users: response.ok && (await response.json())
+      }
+    };
+  }
+</script>
+
 <script lang="ts">
   import FilterableList from '$lib/components/FilterableList.svelte';
-  import supabase from '$lib/supabase';
+  import type { user } from '@prisma/client';
 
-  // TODO: Retrieve counsellor details from database.
-  async function getCounsellors() {
-    let api_counsellors = []
-    try {
+  export let users: user[];
 
-      let {data, error} = await supabase.from('user').select();
+  let counsellors = []
 
-      if (data) {
-
-        for (let i = 0; i < data.length; i++) {
-            api_counsellors.push({
-              title: data[i].fname + ' ' + data[i].lname,
-              description: await getCounsellorsType(data[i].type_id),
-              image: data[i].avatar_url,
-              href: '/admin/employee-details/' + data[i].id
-            });
-        }
-
-      }
-    } catch (error) {
-      alert(error.message);
-    }
-
-    return api_counsellors;
-}
-
-async function getCounsellorsType(typeId) {
-  try {
-
-    let {data, error} = await supabase.from('user_type').select().eq('id', typeId);;
-
-    if (data) {
-      return data[0].name;
-    } else {
-      return '';
-    }
-  } catch (error) {
-    return '';
+  for (let i = 0; i < users.length; i++) {
+      counsellors[counsellors.length] = {
+        title: users[i].fname + ' ' + users[i].lname,
+        description: 'Counsellor',
+        image: users[i].avatar_url,
+        href: '/admin/employee-details/' + users[i].id
+      };
   }
-}
 
-  let promise = getCounsellors();
 </script>
 
   <div class="m-8">
     <h1>Counsellors</h1>
   </div>
   <div class="m-8 h-3/4">
-    {#await promise then counsellors}
     <FilterableList listData={counsellors}/>
-    {/await}
   </div>
--- src/routes/admin/modules.svelte
+++ src/routes/admin/modules.svelte
@@ -1,74 +1,38 @@
+<script context="module" lang="ts">
+  export async function load({ fetch }) {
+    const url = '/api/modules';
+    const response = await fetch(url);
+
+    return {
+      status: response.status,
+      props: {
+        modules: response.ok && (await response.json())
+      }
+    };
+  }
+</script>
+
 <script lang="ts">
   import FilterableList from '$lib/components/FilterableList.svelte';
-  import supabase from '$lib/supabase';
+  import type { chatbot_module } from '@prisma/client';
+
+  export let modules: chatbot_module[];
+  let chatbotModules = [];
   
-  
-  async function getModulesList() {
-    let api_modules = [];
-    try {
-      let {data, error} = await supabase
-        .from('admin_module')
-        .select();
-  
-      if (data) {
-        //console.log(data);
-        for (let i = 0; i < data.length; i++) {
-          let module = {title: data[i].title, description: data[i].description, image: 'https://picsum.photos/id/426/400/600.jpg', dateAccessed: data[i].dateAccessed};
-          //console.log(module);
-          api_modules.push(module);
-          //console.log(api_modules);
-        }
-      }
-      if (error) throw error;
-    } catch (error) {
-      alert(error.message);
-    }
-    return api_modules;
+
+  for (let i = 0; i < modules.length; i++) {
+    chatbotModules[chatbotModules.length] = {
+      title: modules[i].title,
+      description: modules[i].description,
+      image: 'https://picsum.photos/id/426/400/600.jpg',
+      href: '/admin/module-details/' + modules[i].id
+    };
   }
-  let promise = getModulesList()
-
-
-
-  
-
-  // TODO: Retrieve module details from database.
-  /*let modules = [
-    {
-      title: 'Lesson 101: Module Title',
-      description:
-        'Short summary lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vulputate ipsum vel nisi aliquam euismod.',
-      image: 'https://picsum.photos/id/426/400/600.jpg',
-      href: '/admin/module-details/moduleId'
-    },
-    {
-      title: 'Tutorial 102: Module Title',
-      description:
-        'Short summary lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vulputate ipsum vel nisi aliquam euismod.',
-      image: 'https://picsum.photos/id/426/400/600.jpg',
-      href: '/admin/module-details/moduleId'
-    },
-    {
-      title: 'Class 103: Module Title',
-      description:
-        'Short summary lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vulputate ipsum vel nisi aliquam euismod.',
-      image: 'https://picsum.photos/id/426/400/600.jpg',
-      href: '/admin/module-details/moduleId'
-    },
-    {
-      title: 'Module 104: Module Title',
-      description:
-        'Short summary lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vulputate ipsum vel nisi aliquam euismod.',
-      image: 'https://picsum.photos/id/426/400/600.jpg',
-      href: '/admin/module-details/moduleId'
-    }
-  ];*/
 </script>
 
   <div class="m-3">
     <h1>Modules</h1>
   </div>
   <div class="m-3 h-3/4">
-    {#await promise then modules}
-      <FilterableList listData={modules} rectangleOrCircle={true}/>
-    {/await}
+      <FilterableList listData={chatbotModules}/>
   </div>
