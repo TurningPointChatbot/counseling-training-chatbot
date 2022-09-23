2891a70b49de94b63508c1c84bd3bbc47f9dffa4
Krishanu Donahar <kdon0002@student.monash.edu>
2022-09-23 12:53:13 +1000

    fix: home page redirection if no user logged in
    When home page (index) is opened, if the user is not logged in they are now redirected to the login page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,26 @@
-<script lang="ts">
+<!-- <script lang="ts">
   import LoginPage from './login.svelte';
 </script>
 
-<LoginPage />
+<LoginPage /> --> 
+<script context="module" lang="ts">
+import supabase from "$lib/supabase";
+
+  export async function load({ fetch, params }) {
+        // const { user } = session; // since we are using supabase we might be using htat instead.. but will test this out and see what happens 
+        const user = await supabase.auth.user();
+        if (!user) {
+          console.log("since user ain't logged in... I should redirect you");
+          //location.href = '/login';
+            return {
+                status: 302,
+                redirect: "/login"
+            };
+        }
+        else{ // redirect to dashboard
+          console.log("user is logged in ... apparently");
+        }
+        return {};
+  }
+  
+</script>
