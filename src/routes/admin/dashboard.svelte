cdd10caa5ee1a089d0469b560faebef277ffb4cc
Katherine Moffat <kmof0001@student.monash.edu>
2022-09-24 16:15:58 +1000

    fixed the nav issue on dashboard so cards have a link and dont go to signin page and some styling changes

--- src/routes/admin/dashboard.svelte
+++ src/routes/admin/dashboard.svelte
@@ -1,10 +1,23 @@
 <script>
+  import ModuleBigCard from '$lib/components/dashboard/ModuleBigCard.svelte';
+  import ModuleCard from '$lib/components/dashboard/ModuleCard.svelte';
   import BigCard from '$lib/components/dashboard/BigCard.svelte';
   import SmallCard from '$lib/components/dashboard/SmallCard.svelte';
   import ModuleCircleButton from '$lib/components/dashboard/ModuleCircleButton.svelte';
   import supabase from '$lib/supabase';
 
-  // Dummy data to be changed when connected to DB to be deleted
+  export async function load({ fetch}) {
+    const url = `/api/users/counsellors`;
+    const response = await fetch(url);
+    console.log("hi");
+    return {
+      status: response.status,
+      props: {
+        assignments: response.ok && (await response.json())
+      }
+    };
+  }
+
   let counsellorData = [
     'Davos Sand',
     'Evelyn Chua',
@@ -14,34 +27,17 @@
     'Anna Giang'
   ];
 
-  // getting modules from db
-  async function getModulesList() {
-    let api_modules = [];
-    try {
-      let { data, error } = await supabase.from('admin_module').select();
-
-      if (data) {
-        for (let i = 0; i < data.length; i++) {
-          let module = {
-            title: data[i].title,
-            description: data[i].description,
-            image: 'https://picsum.photos/id/426/400/600.jpg',
-            dateAccessed: data[i].dateAccessed
-          };
-          api_modules.push(module);
-        }
-      }
-      if (error) throw error;
-    } catch (error) {
-      alert(error.message);
-    }
-    return api_modules;
-  }
-  let promise = getModulesList(); // list of modules
+  let modulesData = [
+    'Duty of care',
+    'Crisis management',
+    'Lesson 3'
+  ];
 
   // !! list of modules needs to be sorted need to implement somehow sort function
-</script>
 
+
+</script>
+<!-- put URL for each module that is gotten from db in the path -->
 <div class="module-card">
   <BigCard title={'Counsellors'} subTitle={'Recently viewed'}>
     {#each counsellorData as counsellorName}
@@ -50,19 +46,22 @@
     <ModuleCircleButton path="/admin/counsellors" />
   </BigCard>
 
-  {#await promise then modules}
-    <BigCard title={'Training Modules'} subTitle={'Recently Viewed'}>
-      {#each modules as cardItem}
+
+      <BigCard title={'Training Modules'} subTitle={'Recently Viewed'}>
+        {#each modulesData as cardItem}
         <SmallCard
-          image={cardItem.image}
-          alt={'Model of care'}
-          imageClass="object-cover rounded-3xl h-40 w-60 relative"
-          titleButton={cardItem.title}
-        />
-      {/each}
-      <ModuleCircleButton path="/admin/modules" />
-    </BigCard>
+            cardClass = "col-span-2 text-center px-3 py-3"
+            path="/admin/modules"
+            alt={'Model of care'}
+            imageClass=".object-cover h-48 w-96 rounded-3xl relative"
+            titleButton={cardItem}
+            image="https://images.unsplash.com/photo-1584515933487-779824d29309?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&h=225"
+          />
+        {/each}
+        <ModuleCircleButton path="/admin/modules" />
+      </BigCard>
+
 
     <!--<RecentModules listData={modules} /> -->
-  {/await}
-</div>
+
+  </div>
