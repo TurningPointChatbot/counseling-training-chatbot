2b17fff87f3140abd4d5c3dd0a2fa78bdaec5a44
Linton Charles <lintoncharles2000@gmail.com>
2022-10-12 17:45:26 +1100

    feat: Sample jsPDF file

---
+++ src/routes/counsellor/pdf.svelte
@@ -0,0 +1,14 @@
+<script>
+  console.log('PDF page');
+  // require dependencies
+  import { jsPDF } from 'jspdf';
+  var doc = new jsPDF();
+
+  doc.text('Hello world!', 10, 10);
+  doc.output('dataurlnewwindow');
+</script>
+
+<html lang="en" data-theme="cupcake" />
+<div class="m-3">
+  <h1>PDF Page</h1>
+</div>
