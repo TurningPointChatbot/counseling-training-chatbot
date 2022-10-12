6c222a4e4448293a6a0b96a311aa436a5cc5bcd1
Brent <bdia0003@student.monash.edu>
2022-10-12 17:20:00 +1100

    Connected module names and description to backend.

--- src/routes/counsellor/modules.svelte
+++ src/routes/counsellor/modules.svelte
@@ -1,57 +1,74 @@
 <script context="module" lang="ts">
-
-  export async function load({ fetch}) {
-    const url = `/api/chatbot-assignments/user_id=1`;
-    const response = await fetch(url);
-    console.log("hi");
-    return {
-      status: response.status,
-      props: {
-        assignments: response.ok && (await response.json())
-      }
-    };
-  }
-</script>
-
-<script lang="ts">
-  import ModuleRow from '$lib/components/ModuleRow.svelte';
-  import type { chatbot_assignment } from '@prisma/client';
-
-  export let assignments: Array<chatbot_assignment>;
-  let modules: Array<any>;
-
-  function createModuleList() {
-    modules = [];
-    for (let i: number = 0; i < assignments.length; i++) {
-      console.log(assignments.length);
-      modules.push({
-            title: "Module " + assignments[i].cbm_id, // should come from different table
-            description: "Description for module " + assignments[i].cbm_id, // should come from different table
-            cbm_id: assignments[i].cbm_id, 
-            completed: assignments[i].completed,
-            image: 'https://picsum.photos/id/426/400/600.jpg', // should come from different table
-            dateAccessed: '2022-05-25'});
-      }
-    return modules;
-  }
-
-  modules = createModuleList();
-
-</script>
-
-<html lang="en" data-theme="cupcake" />
-  <div class="m-3">
-    <h1>Modules</h1>
-  </div>
-  <div class="m-3 h-3/4">
-    <!-- Generating the list of modules using the ModuleRow.svelte  --> 
-    <ModuleRow listData={modules} rectangleOrCircle={true} />
-     
-  </div>
-
-<!-- UNCOMMENT THIS OUT IF YOU WANT TO SEE RETRIEVED JSON DATA 
-<h1>Json Data</h1>
-{#each modules as module}
-  <p>{JSON.stringify(module)}</p>
-{/each}
--->
+  //TODO: User ID is hardcoded here for now but we will eventually need to implement
+  //functionality to retrieve it from login and pass it here. - Linton
+    export async function load({ fetch}) {
+      const url = `/api/chatbot-assignments/user_id=1`;
+      const response = await fetch(url);
+      const modulesUrl = '/api/modules/chatbot';
+      const modulesResponse = await fetch(modulesUrl);
+      return {
+        status: response.status,
+        moduleStatus: modulesResponse.status,
+        props: {
+          assignments: response.ok && (await response.json()),
+          chatbot_modules: modulesResponse.ok && (await modulesResponse.json())
+        }
+      };
+    }
+  </script>
+  
+  <script lang="ts">
+    import ModuleRow from '$lib/components/ModuleRow.svelte';
+    import type { chatbot_assignment, chatbot_module } from '@prisma/client';
+  
+    export let assignments: Array<chatbot_assignment>;
+    export let chatbot_modules: Array<chatbot_module>;
+  
+    let modules: Array<any>;
+  
+    function findModuleInfo(id: bigint) {
+      for (let i: number = 0; i < chatbot_modules.length; i++) {
+        if(id == chatbot_modules[i].id)
+          return chatbot_modules[i];
+       }
+  
+    }
+  
+    function createModuleList() {
+      modules = [];
+      let tempModule: chatbot_module;
+      for (let i: number = 0; i < assignments.length; i++) {
+        tempModule = findModuleInfo(assignments[i].cbm_id);
+        modules.push({
+              title: tempModule.title, // should come from different table
+              description: tempModule.description, // should come from different table
+              cbm_id: assignments[i].cbm_id, 
+              completed: assignments[i].completed,
+              image: 'https://picsum.photos/id/426/400/600.jpg', // should come from different table
+              dateAccessed: '2022-05-25'});
+        }
+      
+       
+      return modules;
+    }
+  
+    modules = createModuleList();
+  
+  </script>
+  
+  <html lang="en" data-theme="cupcake" />
+    <div class="m-3">
+      <h1>Modules</h1>
+    </div>
+    <div class="m-3 h-3/4">
+      <!-- Generating the list of modules using the ModuleRow.svelte  --> 
+      <ModuleRow listData={modules} rectangleOrCircle={true} />
+       
+    </div>
+  
+  <!-- UNCOMMENT THIS OUT IF YOU WANT TO SEE RETRIEVED JSON DATA 
+  <h1>Json Data</h1>
+  {#each modules as module}
+    <p>{JSON.stringify(module)}</p>
+  {/each}
+  -->
