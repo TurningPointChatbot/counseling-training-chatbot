6ee3feb8c876a21a9fd0881e3647785332e63ac8
Harpo Barrett <61974761+hbar0009@users.noreply.github.com>
2022-10-14 16:40:48 +1100

    fix: typo in filename causing failed imports (#69)

--- src/lib/components/LandingPageTile.svelte
+++ src/lib/components/LandingPageTile.svelte
@@ -2,14 +2,15 @@
   export let imgUrl =
     'https://api.lorem.space/image/furniture?w=400&h=225&hash=33751';
   export let trainingType = '';
-  export let path = '/dashboard'
-  export let description = ""
+  export let path = '/dashboard';
+  export let description = '';
 </script>
 
 <div>
   <a
-  class="bg-gray-100 text-gray-800 text-center rounded-md flex flex-col items-center"
-  href={path}>
+    class="bg-gray-100 text-gray-800 text-center rounded-md flex flex-col items-center"
+    href={path}
+  >
     <img class="rounded-md col-span-1" src={imgUrl} alt="Module" />
     <div class="mb-2">
       <b>
