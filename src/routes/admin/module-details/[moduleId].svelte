21c588edd5b1cd54c56f97a85bf1c54dccf7f9d2
Damien Jap <dami0004@student.monash.edu>
2022-10-16 16:43:49 +1100

    feat: assign modules to counsellors button

--- src/routes/admin/module-details/[moduleId].svelte
+++ src/routes/admin/module-details/[moduleId].svelte
@@ -1,25 +1,68 @@
 <script context="module">
-  export async function load({ params }) {
-    return { props: { moduleId: params.moduleId } };
+  
+  export async function load({ fetch, params }) {
+    const moduleUrl = `/api/modules/chatbot/${params.moduleId}`;
+    const moduleResponse = await fetch(moduleUrl);
+
+    const assignedUrl = `/api/chatbot-assignments/module_id=${params.moduleId}`;
+    const assignedResponse = await fetch(assignedUrl);
+
+    const counsellorsUrl = '/api/users/counsellors';
+    const counsellorsResponse = await fetch(counsellorsUrl);
+
+    return { 
+      moduleStatus: moduleResponse.status,
+      assignedStatus: assignedResponse.status,
+      counsellorStatus: counsellorsResponse.status,
+      props: { 
+        my_chatbot_module: moduleResponse.ok && (await moduleResponse.json()),
+        assigned_list: assignedResponse.ok && (await assignedResponse.json()),
+        moduleId: params.moduleId,
+        counsellors: counsellorsResponse.ok && (await counsellorsResponse.json())
+      } 
+    };
+    
   }
 </script>
 
 <script lang="ts">
-  import FilterableList from '$lib/components/FilterableList.svelte';
+  import Modal from 'svelte-simple-modal';
+  import ModuleDetails from "$lib/components/ModuleDetails.svelte";
+  import {setContext } from 'svelte';
+  import type { chatbot_module, chatbot_assignment, user} from '@prisma/client';
 
   export let moduleId: string;
+  export let my_chatbot_module: chatbot_module;
+  export let assigned_list: chatbot_assignment[];
+  export let counsellors: user[];
+  console.log(assigned_list);
+  let counsellorlist = [];
+  let uniquelist = [];
+  setContext('moduleId', moduleId);
 
-  // TODO: Retrieve module by moduleId.
+  for (let i = 0; i < assigned_list.length; i++) {
+    if (uniquelist.includes(assigned_list[i].user_id)){
+      continue;
+    }
+    else{
+      counsellorlist[counsellorlist.length] = {
+        title: assigned_list[i]["user"].fname + ' ' + assigned_list[i]["user"].lname,
+        description: 'Counsellor',
+        image: assigned_list[i]["user"].avatar_url,
+        href: '/admin/employee-details/' + assigned_list[i].user_id
+      };
+      uniquelist.push(assigned_list[i].user_id);
+    }
+  }
+
   let module = {
-    title: 'Module 101: Empathy',
-    description: `Empathy is the ability to emotionally understand what other people feel, see things from their point of view, and 
-  imagine yourself in their place. Essentially, it is putting yourself in someone else\'s position and feeling what they must be feeling.
-  More and more, we live in bubbles. Most of us are surrounded by people who look like us, vote like us, earn like us, spend money like 
-  us, have educations like us and worship like us. The result is an empathy deficit, and it\'s at the root of many of our biggest problems. It\'s because of how homogeneous people\'s social circles have become, and also because humans naturally hold biases. But researchers have discovered that far from being an immutable trait, empathy can be developed. There are steps people can take to acknowledge their biases and to move beyond their own worldviews to try to understand those held by other people. Bonus: You\'ll make new friends along the way.`
+    title: my_chatbot_module.title,
+    description: my_chatbot_module.description
   };
 
+
   // TODO: Retrieve counsellor module progress from database.
-  let modules = [
+  /*let counsellorlist = [
     {
       title: 'Blake Sand',
       description: 'Junior Counsellor',
@@ -55,21 +98,13 @@
         'https://images.unsplash.com/photo-1595152772835-219674b2a8a6?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160',
       href: '/admin/employee-details/id'
     }
-  ];
+  ];*/
 </script>
 
-<div class="m-5 h-full">
-  <h1>{module.title}</h1>
-  <div class="mt-5">{module.description}</div>
-  <div class="flex items-center justify-center mt-5">
-    <a href="/learning-outcomes/{moduleId}" rel="prefetch">
-      <button class="btn btn-wide btn-success text-white text-lg"
-        >Preview</button
-      >
-    </a>
-  </div>
-  <div class="mt-5"><h2>Assigned Counsellors</h2></div>
-  <div class="mt-3 w-3/4 h-3/5">
-    <FilterableList listData={modules} />
-  </div>
-</div>
+
+
+<Modal>
+  <!-- Module list needs to be a component to provide a context to open modal. 
+    See https://github.com/flekschas/svelte-simple-modal/issues/16 -->
+  <ModuleDetails module = {module} assignedCounsellors={counsellorlist} moduleId = {moduleId} counsellors = {counsellors}/>
+</Modal>
