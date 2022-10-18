f9e2eeb97f01d744340a347d5567917f13f48428
Angus <angus1angus@bigpond.com>
2022-10-18 16:39:02 +1100

    Removed unnecessary functions from user index

--- src/routes/api/users/index.ts
+++ src/routes/api/users/index.ts
@@ -1,9 +1,7 @@
 import { userGET } from './_api';
-import { userFromEmailGET } from './_api';
-import { userModifyPOST} from "./_api";
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
-export async function GETUser() {
+export async function GET() {
   const users = await userGET();
 
   if (users) {
@@ -16,39 +14,5 @@ export async function GETUser() {
 
   return {
     status: 404
-  };
-}
-
-export async function GETUserFromEmail() {
-  const users = await userFromEmailGET();
-
-  if (users) {
-    return {
-      status: 200,
-      headers: {},
-      body: users
-    };
-  }
-
-  return {
-    status: 404
-  };
-}
-
-export async function POST({ request }) {
-  const modified_user = await request.json();
-
-  const adjusted_user = await userModifyPOST(modified_user);
-
-  if (adjusted_user) {
-    return {
-      status: 200,
-      headers: {},
-      body: adjusted_user
-    };
-  }
-
-  return {
-    status: 400
   };
 }
